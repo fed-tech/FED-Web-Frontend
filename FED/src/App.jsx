cf7d0c299968c98f565e6ee246517c0f8f44e9e4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-17 08:09:34 +0530

    Update App.jsx

--- FED/src/App.jsx
+++ FED/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
