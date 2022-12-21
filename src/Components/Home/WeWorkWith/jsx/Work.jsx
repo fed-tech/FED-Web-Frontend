0a7ab2d8f11199e4ccb296e5ac18d801fb0376f7
achyut vardhan <achyutvardhan234@gmail.com>
2022-12-21 23:07:31 +0530

    carousal

--- src/Components/Home/WeWorkWith/jsx/Work.jsx
+++ src/Components/Home/WeWorkWith/jsx/Work.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import Data from "../org.json"
-export default function () {
+import React from "react";
+import Data from "../org.json";
+import "../css/workwith.css";
+import Slider from "react-slick";
+
+export default function Work() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  };
+  console.log(Data);
   return (
     <>
+      <Slider {...settings}>
+        {Data.map((data, i) => (
+          <div key={i}>
+            <img src={data.src} height={100} width={250} alt="" />
+          </div>
+        ))}
+      </Slider>
     </>
-  )
+  );
 }
