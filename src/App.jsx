4c80009bcc58af674215f3e163b0eeae400d9fce
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-22 08:03:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import React from "react";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
+import Events from "./Pages/Events";
+import Podcasts from "./Pages/Podcasts";
+import Team from "./Pages/Team";
+import Testimonial from "./Pages/Testimonial";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
 
@@ -13,6 +19,12 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Podcasts" element={<Podcasts />} />
+          <Route path="/OurTeam" element={<Team />} />
+          <Route path="/Testimonial" element={<Testimonial />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
