7a92247a98bd050478f4ab455d6b6b138cca50b6
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-22 08:46:17 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import Carousel from "./../Components/Home/Carousel";
+import KnowUs from "./../Components/Home/KnowUs";
+import Believe from "./../Components/Home/Believe";
+import SayAboutUs from "./../Components/Home/SayAboutUs";
+import WorkWith from "./../Components/Home/WorkWith";
+import Business from "./../Components/Home/Business";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Carousel />
+      <KnowUs />
+      <Believe />
+      <SayAboutUs />
+      <WorkWith />
+      <Business />
+    </div>
+  );
 }
