e411b123f8ab8889fa92023871035e576770c87b
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2022-12-28 17:30:42 +0530

    update.podcast

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Footer from './component/Footer';
+import Podcasts from './component/Podcasts';
 import Todo from './component/Todo';
 function App() {
   return (<div>
-    
+    <Podcasts />
     <Footer />
   </div>
   );
