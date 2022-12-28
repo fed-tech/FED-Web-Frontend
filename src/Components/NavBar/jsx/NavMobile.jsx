bc1b50fac81afdd0c085d9aa2d999cb2b3b2d54b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-28 14:29:51 +0530

    Update NavMobile.jsx

--- src/Components/NavBar/jsx/NavMobile.jsx
+++ src/Components/NavBar/jsx/NavMobile.jsx
@@ -1,28 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import "../css/MobileNav.css"
-
+import "../css/MobileNav.css";
 
 export default function NavMobile() {
   const [count, setCount] = useState(false);
-  function toggleEvent(){
-    setCount(true)
+  function toggleEvent() {
+    setCount(true);
   }
   return (
     <header>
-    <nav className="mobileNav">
+      <nav className="mobileNav">
         <div id="mobileNavCon">
           <div id="navtoggleDiv">
-            {count ?
-              <div onClick={()=>setCount(false)}>
+            {count ? (
+              <div onClick={() => setCount(false)}>
                 <p className="Cross">X</p>
               </div>
-              :
+            ) : (
               <div className="hamburger" id="ham" onClick={toggleEvent}>
                 <div id="bur1"></div>
                 <div id="bur2"></div>
                 <div id="bur3"></div>
-              </div>}
+              </div>
+            )}
           </div>
           <div className="LogoDiv">
             <img src={"./src/assets/FedLogo.png"}></img>
@@ -31,25 +31,35 @@ export default function NavMobile() {
           <div id="Navdarkright"></div>
         </div>
       </nav>
-      <div id={count? "mobileNavList":"blank"}>
+      <div id={count ? "mobileNavList" : "blank"}>
         <div class="mobileNavListChild">
           <div class="mobileList">
-            <NavLink to="/" className="liTag">Home</NavLink>
+            <NavLink to="/" className="liTag">
+              Home
+            </NavLink>
           </div>
           <div class="mobileList">
-            <NavLink to="/event" className="liTag">Events</NavLink>
+            <NavLink to="/event" className="liTag">
+              Events
+            </NavLink>
           </div>
           <div class="mobileList">
-            <NavLink to="/Podcasts" className="liTag">Podcasts</NavLink>
+            <NavLink to="/Podcasts" className="liTag">
+              Podcasts
+            </NavLink>
           </div>
           <div class="mobileList">
-            <NavLink to="/Team" className="liTag">Our Team</NavLink> 
+            <NavLink to="/Team" className="liTag">
+              Our Team
+            </NavLink>
           </div>
           <div class="mobileList">
-            <NavLink to="/ContactUs" className="liTag">Contact Us</NavLink>
+            <NavLink to="/ContactUs" className="liTag">
+              Contact Us
+            </NavLink>
           </div>
         </div>
       </div>
-      </header>
-  )
+    </header>
+  );
 }
