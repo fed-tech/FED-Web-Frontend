5215fde34aaf5a2fd1a53157959d7100e5619af0
muskan-bajaj <muskanvijaybajaj@gmail.com>
2022-12-28 17:52:21 +0530

    lets talk business

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Carousel from "../Components/Carousel/jsx/Carousel.jsx"
+import LetsTalkBusiness from "../Components/LetsTalkBusiness/jsx/LetsTalkBusiness.jsx";
 
 
 export default function Home() {
   return(<div>
         <Carousel/>
+        <LetsTalkBusiness/>
   </div>);
 }
