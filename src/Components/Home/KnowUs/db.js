f89625ab232cc08156c77d6149aca48ca87164d4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-29 08:15:31 +0530

    achyut

---
+++ src/Components/Home/KnowUs/db.js
@@ -0,0 +1,36 @@
+// https://i.ibb.co/R9v3sHq/how.png
+// https://i.ibb.co/RD4WkcW/what.png
+// https://i.ibb.co/cyW92Vc/why.png
+import why from "../../../Img/why.png"
+import what from "../../../Img/what.png"
+import how from "../../../Img/how.png"
+
+export default [
+  {
+    class: "whyDiv",
+    img: `${why}`,
+    for: "Why?",
+    content1: "We aim to nurture entrepreneurship through",
+    content2: "creative, authentic, and efficient techniques.",
+    content3: "",
+  },
+  {
+    class: "HowDiv",
+    img: `${how}`,
+    for: "How?",
+    content1:
+      "Strong co-operation between members that our organization instills is how we can accomplish all of this.",
+    content2:
+      " There is no I, only we. We exist as a team, function as a team,",
+    content3: "and execute as a team.",
+  },
+  {
+    class: "WhatDiv",
+    img: `${what}`,
+    for: "What?",
+    content1: "Through our social media handles, we",
+    content2:
+      " arrange informative podcasts, high-quality events, and inspiring and thought-provoking material.",
+    content3: "",
+  },
+];
---
+++ src/Components/Home/Testimonial/card-detail.js
@@ -0,0 +1,12 @@
+export default [ 
+    {
+      testpTag : '“Amazing tournament... please inform any further tournaments youconduct.”',
+      userName : 'Ayan',
+      userPost : 'FED H2 Invitational'
+    },
+    {
+      testpTag : '“It was very informative indeed. i got to know much about digital art and how to make it. loved it.”',
+      userName : 'Avignya Kashyap',
+      userPost : 'NFT workshop'
+    }
+  ];
