b6010808d700948f08a65f5963e3c859d8a3ab3c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-29 09:23:16 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Img from "../Components/Home/Work";
 import KnowUsCom from "../Components/Home/KnowUs";
 import BelieveCom from "./../Components/Home/BelieveCom";
 import Testimonial from "./../Components/Home/Testimonial";
+import WorkWith from "./../Components/Home/WorkWith";
 
 // css
 import "../Components/Home/css/space.css";
@@ -17,29 +18,7 @@ export default function Home() {
       <KnowUsCom />
       <BelieveCom />
       <Testimonial />
-      <section id="work">
-        <div className="WeWorkWith">
-          <p className="WeWorkWithPTag">
-            We <span>work</span> with...
-          </p>
-          {/* Slider */}
-          <div className="slideParentDiv">
-            <div className="slider">
-              <div className="slide-track">
-                <Img />
-              </div>
-            </div>
-          </div>
-          <div className="pTagDivWork">
-            <p className="workptagDes">
-              We wish to express our deepest gratitude to our sponsors for their
-              steadfast support and generosity, and for their dedication to
-              making a difference in the entrepreneurial community by supporting
-              grassroots initiatives.
-            </p>
-          </div>
-        </div>
-      </section>
+      <WorkWith />
     </>
   );
 }
