11dbbd7e7f8056d9290591be3c1457a552eb2c9f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-29 09:16:04 +0530

    Update KnowUs.jsx

--- src/Components/Home/KnowUs.jsx
+++ src/Components/Home/KnowUs.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import Content from "./../../Components/Home/Content";
+
+//
+import knowus from "./../../Data/db";
+import women from "./../../Img/image 7.png";
 
 export default function KnowUs() {
-  return <div>KnowUs</div>;
+  return (
+    <>
+      <section id="KnowUs">
+        <div className="KnowUsmDiv">
+          <p id="KnowUspTag">
+            <span className="KnowUspTag">Know us </span>
+            better...
+          </p>
+          <img src={women} alt="" className="womenWithALaptop" />
+          <p id="womenBelowText">
+            Federation of Entrepreneurship Development (FED) is a group of KIIT
+            TBI students that want to encourage individuals with entrepreneurial
+            aspirations by bringing all prospective startup ideas together on
+            one platform. By planning events to promote entrepreneurship
+            awareness, FED connects you with the best resources, contacts, and
+            mentors.
+          </p>
+        </div>
+        {/* Why_What_How */}
+        <div className="WhyHowWhatDiv">
+          <Content knowus={knowus[0]} />
+          <Content knowus={knowus[1]} />
+          <Content knowus={knowus[2]} />
+        </div>
+        <div className="space1"></div>
+      </section>
+    </>
+  );
 }
