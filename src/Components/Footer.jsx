5cdb96491a02296339467478c1ac34acbecb2dca
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-29 08:05:27 +0530

    delete

--- src/Components/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Css/Footer.css";
-
-export default function Footer() {
-  return <div>Footer</div>;
-}
--- src/Pages/Testimonial.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Testimonial() {
-  return <div>Testimonial</div>;
-}
