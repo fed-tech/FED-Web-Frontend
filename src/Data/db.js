ada6c0eb4d2603430b98dd52cf5ddb9b365278f7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-29 08:51:11 +0530

    Create db.js

---
+++ src/Data/db.js
@@ -0,0 +1,33 @@
+import why from "../../../Img/why.png";
+import what from "../../../Img/what.png";
+import how from "../../../Img/how.png";
+
+export default [
+  {
+    class: "whyDiv",
+    img: `${why}`,
+    for: "Why?",
+    content1: "We aim to nurture entrepreneurship through",
+    content2: "creative, authentic, and efficient techniques.",
+    content3: "",
+  },
+  {
+    class: "HowDiv",
+    img: `${how}`,
+    for: "How?",
+    content1:
+      "Strong co-operation between members that our organization instills is how we can accomplish all of this.",
+    content2:
+      " There is no I, only we. We exist as a team, function as a team,",
+    content3: "and execute as a team.",
+  },
+  {
+    class: "WhatDiv",
+    img: `${what}`,
+    for: "What?",
+    content1: "Through our social media handles, we",
+    content2:
+      " arrange informative podcasts, high-quality events, and inspiring and thought-provoking material.",
+    content3: "",
+  },
+];
