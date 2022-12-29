26e3e389f4686960af77fed4c77bfda3381266ae
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-12-29 08:55:17 +0530

    links changed

--- src/Data/db.js
+++ src/Data/db.js
@@ -1,7 +1,7 @@
 export default [
   {
     class: "whyDiv",
-    img: `${why}`,
+    img: `https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/63ad0806268d1877e9786678_why.png`,
     for: "Why?",
     content1: "We aim to nurture entrepreneurship through",
     content2: "creative, authentic, and efficient techniques.",
@@ -9,7 +9,7 @@ export default [
   },
   {
     class: "HowDiv",
-    img: `${how}`,
+    img: `https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/63ad080e7a881d28eb9cacc7_how.png`,
     for: "How?",
     content1:
       "Strong co-operation between members that our organization instills is how we can accomplish all of this.",
@@ -19,7 +19,7 @@ export default [
   },
   {
     class: "WhatDiv",
-    img: `${what}`,
+    img: `https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/63ad0800a2e57b8ce3c83da8_what.png`,
     for: "What?",
     content1: "Through our social media handles, we",
     content2:
