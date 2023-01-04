1a7622ac46eb0172ae6e05c1e17076bdc6ba92b7
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-01-04 16:38:45 +0530

    youtube

--- src/Components/Podcasts/Main.jsx
+++ src/Components/Podcasts/Main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./css1/podcasts.css";
+import { Navigate } from "react-router-dom";
 function Main(props)
 {
+   const [goToYoutube, setGoToYoutube] = React.useState(false);
+   React.useEffect(() => {
+      if (goToYoutube) {
+      return <Navigate to="/Youtube" />;
+   }})
    return(
     <section id="podcasts">
        <div className="KnowUsmDiv">
@@ -13,7 +19,7 @@ function Main(props)
         <p className="para3">
         The FEDpreneur Show is our podcast series in which we host India's most successful entrepreneurs and strive to uncover their secrets to success, learning, and entertainment! Every conversation on The FEDpreneur Show offers intellectual, educational, and extreme learning opportunities to the audience. It's about a visionary entrepreneur session with a group of multi-talented and experienced business owners. It would aid our young brains to stand strong in marketing and startup endeavors with our podcasts at "The FEDpreneur Show," where we host India's best success stories and aim to dig out their secrets to success, learning, and enjoyment!
         </p>
-         <button className="cover-btn">Tune in Now</button>
+         <button className="cover-btn" onClick={() => {setGoToYoutube(true)}}>Tune in Now</button>
        </div> 
     </section>
 
--- src/Pages/Podcasts.jsx
+++ src/Pages/Podcasts.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Main from "../Components/Podcasts/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Youtube from "../Components/Podcasts/Youtube";
 function Podcasts()
 {
  return(
     <>
+    <BrowserRouter>
         <Main />
+        <Routes>
+        <Route path="/Youtube" element={<Youtube/>} />
+        </Routes>
+        </BrowserRouter>
     </>
  )
 }
