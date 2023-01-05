3d1b1a5974e95f79e1a13abcd3a3c125c5566769
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-01-05 09:13:21 +0530

    youtube

--- src/Components/Podcasts/Main.jsx
+++ src/Components/Podcasts/Main.jsx
@@ -1,15 +1,15 @@
-import React, {useState,useEffect}from "react";
+import { useState } from "react";
+import Youtube from "./Youtube";
+import Backdrop from "./Backdrop";
 import "./css1/podcasts.css";
-import { useNavigation } from "react-router-dom";
 function Main(props)
 {
-   const navigation = useNavigation();
+   
    const [goToYoutube, setGoToYoutube] = useState(false);
-   useEffect(() => {
-    if (goToYoutube) {
-      navigation("/Youtube")
-    }  
-   },[goToYoutube])
+   function newState()
+   {
+      setGoToYoutube(true);
+   }
    return(
     <section id="podcasts">
        <div className="KnowUsmDiv">
@@ -21,7 +21,9 @@ function Main(props)
         <p className="para3">
         The FEDpreneur Show is our podcast series in which we host India's most successful entrepreneurs and strive to uncover their secrets to success, learning, and entertainment! Every conversation on The FEDpreneur Show offers intellectual, educational, and extreme learning opportunities to the audience. It's about a visionary entrepreneur session with a group of multi-talented and experienced business owners. It would aid our young brains to stand strong in marketing and startup endeavors with our podcasts at "The FEDpreneur Show," where we host India's best success stories and aim to dig out their secrets to success, learning, and enjoyment!
         </p>
-         <button className="cover-btn" onClick={() => {setGoToYoutube(true)}}>Tune in Now</button>
+         <button className="cover-btn" onClick={newState}>Tune in Now</button>
+         {goToYoutube && <Youtube />}
+         {goToYoutube && <Backdrop />}
        </div> 
     </section>
 
--- src/Components/Podcasts/Youtube.jsx
+++ src/Components/Podcasts/Youtube.jsx
@@ -3,8 +3,36 @@ import "./css1/youtube.css";
 function Youtube(props)
 {
     return(
-        <div>
-
+        <div className="modal">
+         <div className="descriptiondiv">
+           <h4 className="heading1">Description:</h4>
+           <p className="firstdiv">
+           The FEDpreneur Show is a Federation KIIT podcast series in which we host India's most successful entrepreneurs and attempt to unearth their secrets to success while learning entertainingly! Every conversation on The FEDpreneur Show offers the audience intellectual, educational, and extraordinary learning possibilities. It's about a visionary entrepreneur session with a group of multi-talented and experienced business owners. With a forthcoming podcast, "The FEDpreneur Show," where we host India's top success stories and strive to dig out their secrets to success, learning, and fun, it would help our young minds stand firm in marketing and startup endeavors!
+           </p>
+         </div>
+         <div>
+            <h4 className="heading2">Watch More</h4>
+            <div className="watchmore">
+                <div className="div1">
+                    <div className="underdiv1">
+                        <div className="items">
+                            <div className="imagediv">
+                              <img src="https://i.ytimg.com/vi/tqGsqwp67-g/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLAajJb7ww7UYPUEd4PIFTbhXOGZCg"></img>
+                            </div>
+                            <div className="eti">
+                                <h4 className="empowering">Empowering through Innovation</h4>
+                                <h4 className="ep">EP 03</h4>
+                                <p className="">
+                                The FEDpreneur Show is a Federation KIIT podcast series in which we host India's most successful entrepreneurs and attempt to unearth their secrets to success while learning entertainingly! Every conversation on The FEDpreneur Show offers the audience intellectual, educational, a...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="div2"></div>
+                <div className="div3"></div>
+            </div>
+         </div>
         </div>
     );
 }
