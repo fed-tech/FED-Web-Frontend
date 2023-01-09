7ec5d8c8321a4e1e4b3a2e55aed698dd73959a6c
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-01-09 21:06:29 +0530

    youtube.

--- src/Components/Podcasts/Youtube.jsx
+++ src/Components/Podcasts/Youtube.jsx
@@ -7,6 +7,21 @@ function Youtube(props)
 {
     return(
         <div className="mainbody">
+        <div className="container1">
+        <div className="container2">
+           <iframe
+           width="100%"
+           height="480"
+           src="https://www.youtube.com/embed/tqGsqwp67-g"
+           title="Empowering through Innovation | EP 03 | The FEDpreneur Show"
+           frameborder="0"
+           allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope; picture-in-picture"
+           allowfullscreen
+           >
+
+           </iframe>
+        </div>
+        </div>
          <div className="descriptiondiv">
            <h4 className="heading1">Description:</h4>
            <p className="firstdiv">
@@ -42,35 +57,35 @@ function Youtube(props)
                                 <h4 className="empowering">Skills Always Reign Supreme</h4>
                                 <h4 className="ep">EP 02</h4>
                                 <p className="paragraph">
-                                Hello Viewers! 
-                                <br/>
-      The FEDpreneur Show is our new podcast series in which we host India's most successful entrepreneurs and strive to uncover their secrets to success, learning, and entertainment! Every conversation on The FEDpreneur Show provides the viewer with intellectual, educational, and extreme learning opportunities. It is about a visionary entrepreneur session with several multi-talented and experienced entrepreneurs. It would aid our young brains to stand strong in marketing and startup endeavors with an upcoming podcast, "The FEDpreneur Show," where we host India's best success stories and aim to dig out their secrets to success, learning, and enjoyment!
-      <br/>
+                                 Hello Viewers! 
+                                 <br/>
+                                   The FEDpreneur Show is our new podcast series in which we host India's most successful entrepreneurs and strive to uncover their secrets to success, learning, and entertainment! Every conversation on The FEDpreneur Show provides the viewer with intellectual, educational, and extreme learning opportunities. It is about a visionary entrepreneur session with several multi-talented and experienced entrepreneurs. It would aid our young brains to stand strong in marketing and startup endeavors with an upcoming podcast, "The FEDpreneur Show," where we host India's best success stories and aim to dig out their secrets to success, learning, and enjoyment!
+                                 <br/>
 
-      Today's audio has an in-depth chat with Nimish Keshri about building start-ups in India, their philosophy, secrets, ups and downs along the way, and many other fascinating information.
-      <br/>
+                                 Today's audio has an in-depth chat with Nimish Keshri about building start-ups in India, their philosophy, secrets, ups and downs along the way, and many other fascinating information.
+                                  <br/>
 
-      Nimish Keshri is the founder and CEO of the EdTech business SkillVERTEX. SkillVERTEX is an online learning platform where students may explore and select from a variety of courses. SkillVERTEX provides their customers with a placement programme to assist them develop their abilities in the workplace.
-      <br/> 
+                                 Nimish Keshri is the founder and CEO of the EdTech business SkillVERTEX. SkillVERTEX is an online learning platform where students may explore and select from a variety of courses. SkillVERTEX provides their customers with a placement programme to assist them develop their abilities in the workplace.
+                                 <br/> 
 
-      Follow us on : 
-      <br/>
+                                 Follow us on : 
+                                 <br/>
 
-      Instagram: <a href="https://www.instagram.com/fedkiit/" target="_blank">https://www.instagram.com/fedkiit/</a>  
-      <br/>
+                                  Instagram: <a href="https://www.instagram.com/fedkiit/" target="_blank">https://www.instagram.com/fedkiit/</a>  
+                                  <br/>
 
-      LinkedIn : <a href="https://www.linkedin.com/company/fedkiit" target="_blank"> https://www.linkedin.com/company/fedkiit </a> 
-      <br/>
+                                  LinkedIn : <a href="https://www.linkedin.com/company/fedkiit" target="_blank"> https://www.linkedin.com/company/fedkiit </a> 
+                                 <br/>
 
       
-      Guest of honor: Nimish Keshri (Instagram: <a href="https://www.instagram.com/who_nk/?utm_medi..." target="_blank">https://instagram.com/who_nk?utm_medi...</a> )
-      <br/>
+                                    Guest of honor: Nimish Keshri (Instagram: <a href="https://www.instagram.com/who_nk/?utm_medi..." target="_blank">https://instagram.com/who_nk?utm_medi...</a> )
+                                 <br/>
 
-      Podcaster: Aniket Patro (Instagram:<a href="https://www.instagram.com/ankp04/?utm_medi..." target="_blank"> https://instagram.com/ankp04?utm_medi...</a> ) 
-      <br/>
+                                   Podcaster: Aniket Patro (Instagram:<a href="https://www.instagram.com/ankp04/?utm_medi..." target="_blank"> https://instagram.com/ankp04?utm_medi...</a> ) 
+                                  <br/>
 
-      SkillVERTEX ( <a href="https://skillvertex.in">https://skillvertex.in</a>)
-                                </p>
+                                 SkillVERTEX ( <a href="https://skillvertex.in">https://skillvertex.in</a>)
+                                  </p>
                             </div>
                         </div>
                     </div>
@@ -89,60 +104,60 @@ function Youtube(props)
                                 <br/>
       
 
-      The FEDpreneur Show is our new podcast series in which we host India's most successful entrepreneurs and strive to uncover their secrets to success, learning, and entertainment! Every conversation on The FEDpreneur Show provides the viewer with intellectual, educational, and extreme learning opportunities. It is about a visionary entrepreneur session with several multi-talented and experienced entrepreneurs. It will aid our young brains to stand strong in marketing and startup endeavours with an upcoming podcast, "The FEDpreneur Show," where we host India's best success stories and aim to dig out their secrets to success, learning, and enjoyment!
-      <br/>
+                                The FEDpreneur Show is our new podcast series in which we host India's most successful entrepreneurs and strive to uncover their secrets to success, learning, and entertainment! Every conversation on The FEDpreneur Show provides the viewer with intellectual, educational, and extreme learning opportunities. It is about a visionary entrepreneur session with several multi-talented and experienced entrepreneurs. It will aid our young brains to stand strong in marketing and startup endeavours with an upcoming podcast, "The FEDpreneur Show," where we host India's best success stories and aim to dig out their secrets to success, learning, and enjoyment!
+                                <br/>
 
-      Today's audio episode includes an in-depth discussion with Niket Raj Dwivedi on developing start-ups in India, their philosophy, their secrets, and many other topics.
-      <br/>
+                               Today's audio episode includes an in-depth discussion with Niket Raj Dwivedi on developing start-ups in India, their philosophy, their secrets, and many other topics.
+                               <br/>
       
 
-      FED's creator, Niket Raj Dwivedi, is also the CEO of The Write Order, India's fastest growing publishing business situated in Bangalore, Karnataka. He is a wonderful writer, an incredible poet, and a successful entrepreneur, the creator of the Federation of Entrepreneurship-KIIT, also known as the KIIT's Lone Wolf. This entrepreneurial interview is extremely useful since it gives a step-by-step instruction on how to launch a firm with only a business idea. The Indian startup landscape is unique, and this video might help you learn more.
-      <br/>
+                               FED's creator, Niket Raj Dwivedi, is also the CEO of The Write Order, India's fastest growing publishing business situated in Bangalore, Karnataka. He is a wonderful writer, an incredible poet, and a successful entrepreneur, the creator of the Federation of Entrepreneurship-KIIT, also known as the KIIT's Lone Wolf. This entrepreneurial interview is extremely useful since it gives a step-by-step instruction on how to launch a firm with only a business idea. The Indian startup landscape is unique, and this video might help you learn more.
+                               <br/>
 
-      This Indian podcast is hopefully going to spread a lot of knowledge and education amongst the various segments of the society.
-      <br/>
+                               This Indian podcast is hopefully going to spread a lot of knowledge and education amongst the various segments of the society.
+                               <br/>
       
 
-      Ambar Bishun & Niket Raj Dwivedi are here to discuss the grind before the success of The Write Order.
-      <br/>
+                              Ambar Bishun & Niket Raj Dwivedi are here to discuss the grind before the success of The Write Order.
+                              <br/>
       
 
-      Stay connected with the Federation of Entrepreneurship Development-KIIT via our social media platforms to know more about us.
-      <br/>
+                              Stay connected with the Federation of Entrepreneurship Development-KIIT via our social media platforms to know more about us.
+                               <br/>
 
-      Follow us on :
-      <br/>
+                              Follow us on :
+                               <br/>
       
 
-      Instagram: <a href="https://www.instagram.com/fedkiit/" target="_blank">https://www.instagram.com/fedkiit/</a>  
-      <br/> 
+                              Instagram: <a href="https://www.instagram.com/fedkiit/" target="_blank">https://www.instagram.com/fedkiit/</a>  
+                               <br/> 
 
-      LinkedIn : <a href="https://www.linkedin.com/company/fedkiit" target="_blank">https://www.linkedin.com/company/fedkiit</a>
-      <br/>  
+                               LinkedIn : <a href="https://www.linkedin.com/company/fedkiit" target="_blank">https://www.linkedin.com/company/fedkiit</a>
+                              <br/>  
 
-      Facebook: <a href="https://www.facebook.com/fedkiit" target="_blank">https://www.facebook.com/fedkiit </a> 
-      <br/>  
+                               Facebook: <a href="https://www.facebook.com/fedkiit" target="_blank">https://www.facebook.com/fedkiit </a> 
+                               <br/>  
       
 
-      Guest of honour: Niket Raj Dwivedi (Instagram: <a href="https://www.instagram.com/niketrajdwi.../" target="_blank">https://www.instagram.com/niketrajdwi...</a>)
-      <br/> 
+                              Guest of honour: Niket Raj Dwivedi (Instagram: <a href="https://www.instagram.com/niketrajdwi.../" target="_blank">https://www.instagram.com/niketrajdwi...</a>)
+                              <br/> 
 
-      Podcaster: Ambar Bishun (Instagram: <a href="https://www.instagram.com/august_boy_99/" target="_blank">https://www.instagram.com/august_boy_99/</a>) 
-      <br/>
+                               Podcaster: Ambar Bishun (Instagram: <a href="https://www.instagram.com/august_boy_99/" target="_blank">https://www.instagram.com/august_boy_99/</a>) 
+                              <br/>
 
-      Host: Mayank Rai (Instagram: <a href="https://www.instagram.com/mayankkkk._0/" target="_blank">https://www.instagram.com/mayankkkk._0/</a>) 
-      <br/>
+                             Host: Mayank Rai (Instagram: <a href="https://www.instagram.com/mayankkkk._0/" target="_blank">https://www.instagram.com/mayankkkk._0/</a>) 
+                                <br/>
 
-      The Write Order (Instagram: <a href="https://www.instagram.com/thewriteorder/" target="_blank">https://www.instagram.com/thewriteorder/</a>)
-      <br/>
-      <br/> 
+                             The Write Order (Instagram: <a href="https://www.instagram.com/thewriteorder/" target="_blank">https://www.instagram.com/thewriteorder/</a>)
+                             <br/>
+                             <br/> 
 
       
 
-      Cheers
-      <br/> 
+                             Cheers
+                             <br/> 
 
-      TEAM FED
+                             TEAM FED
                                 </p>
                             </div>
                         </div>
---
+++ src/component/Youtube.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Youtube(props)
+{
+    return(
+        <section id="YOUTUBE">
+        <div className="yt">
+        </div>
+        </section>
+
+    );
+}
+export default Youtube;
