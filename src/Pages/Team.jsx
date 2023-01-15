91d29667207ea46ea436ae61e57f24e4b29d7815
achyut vardhan <achyutvardhan234@gmail.com>
2023-01-15 23:58:56 +0530

    team page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -1,10 +1,117 @@
 import React from 'react'
-
+import '../Components/Team/Team.css'
+import '../Components/Team/loading.css'
+import '../Components/Team/Team-Card/Core.jsx'
 export default function Team() {
     console.log("object");
   return (
     <>
-    <h1>team</h1>
+    <section id='OurTeam'>
+       {/* Core */}
+       <div className="TeamCore">
+          <p className="TeamName">Meet Our <span>Team.</span></p>
+          <p className="TeamNameSubTitle">
+            We are a tight-knit community of passionate people devoted to
+            bringing about vibrant and awe-inspiring changes in the field of
+            Entrepreneurship. The pillars of our crew are the Marketing group,
+            the Creative group, the Technical group, and the Operations group.
+          </p>
+          <div id="CoreTeam">
+            <div className="centerLoader">
+              <div className="arc" />
+              <h1 className="loadingSpanH1">
+                <span className="loadingSpan">Loading</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="space" />
+        {/* Technical */}
+        <div className="TeamCreative">
+          <p className="TeamName">Team <span>Technical</span></p>
+          <p className="TeamNameSubTitle">
+            Those who help us design, analyze, troubleshoot, diagnose and
+            resolve technicalities in FED!
+          </p>
+          <div id="TechnicalTeam">
+            <div className="centerLoader">
+              <div className="arc" />
+              <h1 className="loadingSpanH1">
+                <span className="loadingSpan">Loading</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="space" />
+        {/* Creative */}
+        <div className="TeamCreative">
+          <p className="TeamName">Team <span>Creative</span></p>
+          <p className="TeamNameSubTitle">
+            Those who help us illustrate, create, design, address, showcase,
+            and introduce FED as a whole- behind the veil!
+          </p>
+          <div id="CreativeTeam">
+            <div className="centerLoader">
+              <div className="arc" />
+              <h1 className="loadingSpanH1">
+                <span className="loadingSpan">Loading</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="space" />
+        {/* Marketing */}
+        <div className="TeamCreative">
+          <p className="TeamName">Team <span>Marketing</span></p>
+          <p className="TeamNameSubTitle">
+            Those who help us strategize, develop, promote, grow and market
+            FED's endeavor!
+          </p>
+          <div id="MarketingTeam">
+            <div className="centerLoader">
+              <div className="arc" />
+              <h1 className="loadingSpanH1">
+                <span className="loadingSpan">Loading</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="space" />
+        {/* Operations */}
+        <div className="TeamCreative">
+          <p className="TeamName">Team <span>Operations</span></p>
+          <p className="TeamNameSubTitle">
+            Those who help us plan, strategize, commemorate, organize and
+            operate FED's initiatives!
+          </p>
+          <div id="OperationsTeam">
+            <div className="centerLoader">
+              <div className="arc" />
+              <h1 className="loadingSpanH1">
+                <span className="loadingSpan">Loading</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="space" />
+        {/* Alumni */}
+        <div className="TeamCreative">
+          <p className="TeamName">Our <span>Alumni</span></p>
+          <p className="TeamNameSubTitle">
+            {/* Those who help us plan, strategize, commemorate, organize and
+          operate FED's initiatives! */}
+          </p>
+          <div id="AlumniTeam">
+            <div className="centerLoader">
+              <div className="arc" />
+              <h1 className="loadingSpanH1">
+                <span className="loadingSpan">Loading</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="space"></div>
+    </section>
     </>
   )
 }
