4c063ae5402da9d617c22df655da1cd1c06bc49f
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-01-23 03:16:28 +0530

    .

--- src/Components/Alumni/OurAlumni.jsx
+++ src/Components/Alumni/OurAlumni.jsx
@@ -6,23 +6,39 @@ function OurAlumni() {
     <section className="alumni">
       <div className="main_body">
         <div className="ouralumni">
-          <p className="para1">
-            Our <span className="para2">Alumni.</span>
+          <p className="para1_">
+            Our <span className="para2_">Alumni.</span>
           </p>
         </div>
-        <p className="para3">
+        <p className="para3_">
           Effort that is done by you all for the development of our society
         </p>
       </div>
-      {Card.map((card) => {
-        return (
-          <div className="box">
-            <div className="box1">
-              <img src={card.image} className="image1" alt="alumni"></img>
+      <div className="bottomcontainer_">
+        {Card.map((card) => {
+          return (
+            <div className="box_">
+              <div className="box1_">
+                <img src={card.image_} className="image_1" alt="alumni"></img>
+              </div>
+              <div className="child_">
+                <div className="content_">
+                  <h1 className="heading_1">{card.title_}</h1>
+                  <h3>{card.subtitle}</h3>
+                  <p>{card.para_}</p>
+                  <div className="icon_">
+                    <img
+                      src={card.icon_}
+                      className="iconimage"
+                      alt="icons"
+                    ></img>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </section>
   );
 }
