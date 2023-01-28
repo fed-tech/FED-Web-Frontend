a98d0bb21d944b7ab0c4f2cdb708fa3ffb0fbcf6
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-01-28 13:17:10 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Footer from './component/Footer';
+import Podcasts from './component/Podcasts';
+import Todo from './component/Todo';
+import Youtube from './component/Youtube';
+function App() {
+  return (
+  <div>
+    <Podcasts />
+   <Router>
+    <Routes>
+     <Route path="/Youtube" element={<Youtube />} />
+    </Routes>
+   </Router>
+    <Footer />
+  </div>
+  );
+}
+
+export default App;
