cf0e2b8cd099dec3f0bea882ba927425f899423b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-29 21:50:31 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// css
+import ECss from "./Css/Error.module.css";
+
 export default function Error() {
-  return <div>Error 404</div>;
+  return <div className={ECss.mDiv}>Error 404</div>;
 }
