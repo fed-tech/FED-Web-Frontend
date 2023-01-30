4dbbbb65ec22d7177ca3972934beb3072e826e90
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-30 11:35:13 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,7 +8,7 @@ export default function Nav() {
       <nav className="desktopNav">
         <div className="navmDiv">
           <div className="LogoDiv">
-            <img src={"./src/assets/FedLogo.png"}></img>
+            <img src="https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/62eeaa9927e6aea4ff13590e_FedLogo.png"></img>
             <p className="LogoFED">FED</p>
           </div>
           <div className="listDiv">
