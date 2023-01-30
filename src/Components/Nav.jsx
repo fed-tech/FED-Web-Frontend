c3f03e4beb68fbce0cb1516195138c502805ef6f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-30 11:37:49 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./css/DesktopNav.css";
 
 export default function Nav() {
@@ -7,10 +7,12 @@ export default function Nav() {
     <header className="Navigation">
       <nav className="desktopNav">
         <div className="navmDiv">
-          <div className="LogoDiv">
-            <img src="https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/62eeaa9927e6aea4ff13590e_FedLogo.png"></img>
-            <p className="LogoFED">FED</p>
-          </div>
+          <Link to="/" className="LinkStyle">
+            <div className="LogoDiv">
+              <img src="https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/62eeaa9927e6aea4ff13590e_FedLogo.png"></img>
+              <p className="LogoFED">FED</p>
+            </div>
+          </Link>
           <div className="listDiv">
             <ul id="NavUl">
               <li>
