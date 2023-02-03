4d11e21f186dd5cec10f8ae1bc4f5c54c5fe2983
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-02-03 10:16:09 +0530

    .

--- src/Components/Home/Seeall.jsx
+++ src/Components/Home/Seeall.jsx
@@ -8,7 +8,11 @@ function Seeall() {
         {Wcard.map((element) => {
           return (
             <div className="mcard">
-              <div className="cardbox"></div>
+              <div className="cardbox">
+                <p className="comment">{element.comment}</p>
+                <p className="text_2">{element.speaker_name}</p>
+                <p className="text_3">{element.program_}</p>
+              </div>
             </div>
           );
         })}
