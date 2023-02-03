9c63eff9fe468a0909f3fb4729f88a9fcf1a143a
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-02-03 22:11:16 +0530

    .

--- src/Components/Home/Err.jsx
+++ src/Components/Home/Err.jsx
@@ -3,8 +3,9 @@ import "./css/err.css";
 function Err() {
   return (
     <div>
-      <div className="err">
+      <div className="err_">
         <p className="err_box">404</p>
+        <p className="err_message">Not Found</p>
       </div>
     </div>
   );
