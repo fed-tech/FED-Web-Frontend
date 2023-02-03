f0443ea018e40d4aef333edef0f3b3c06269c458
NavyaVerma139 <113771639+NavyaVerma139@users.noreply.github.com>
2023-02-03 18:27:24 +0530

    testimonialseall

--- src/Components/Home/Seeall.jsx
+++ src/Components/Home/Seeall.jsx
@@ -10,6 +10,8 @@ function Seeall() {
             <div className="mcard">
               <div className="cardbox">
                 <p className="cmt">{Wcard.comment}</p>
+                <p className="sp">{Wcard.speaker_name}</p>
+                <p className="prg">{Wcard.program_}</p>
               </div>
             </div>
           );
