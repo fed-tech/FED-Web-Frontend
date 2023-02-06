de150d7bd80f3f598b7b70164c916eb054854ec0
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-02-06 22:31:13 +0530

    Update Testimonial.jsx
    Co-Authored-By: NavyaVerma139 <113771639+NavyaVerma139@users.noreply.github.com>

--- src/Pages/Testimonial.jsx
+++ src/Pages/Testimonial.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
+// css
+import "./css/seeall.css";
+
+// json
+import Wcard from "./testimonial.json";
+
 export default function Testimonial() {
-  return <div>Testimonial</div>;
+  return (
+    <div className="seeall_page">
+      <div className="sub_div">
+        {Wcard.map((element) => {
+          return (
+            <div className="mcard">
+              <div className="cardbox">
+                <p className="cmt">{element.comment}</p>
+                <p className="sp">{element.speaker_name}</p>
+                <p className="prg">{element.program_}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
