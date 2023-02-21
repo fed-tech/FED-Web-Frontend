3e12c2a37d686e5d417ef118d6561934c9ad62c9
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-02-22 02:21:32 +0530

    .

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Css/err.css";
-import Errimage from "./Css/errimage/Layer3.svg";
+// import Errimage from "./../Pages/Css/errimage/Layer3.svg";
 function Errorpage() {
   return (
     <div className="bg">
@@ -9,11 +9,11 @@ function Errorpage() {
       <p className="the_page">
         The page you requested could not be found.
         <br />
-        We're working on it :
+        We're working on it :)
       </p>
-      <div className="cycle_image">
+      {/* <div className="cycle_image">
         <img src={Errimage} className="cycleimage" alt=""></img>
-      </div>
+      </div> */}
     </div>
   );
 }
