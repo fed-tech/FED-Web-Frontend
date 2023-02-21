b31568875f261dd229cc2618b1b2735fbe72a70e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-02-22 02:30:49 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Css/err.css";
-// import Errimage from "./../Pages/Css/errimage/Layer3.svg";
+import Errimage from "./../assets/Layer 3.svg";
+
 function Errorpage() {
   return (
     <div className="bg">
@@ -11,9 +12,9 @@ function Errorpage() {
         <br />
         We're working on it :)
       </p>
-      {/* <div className="cycle_image">
+      <div className="cycle_image">
         <img src={Errimage} className="cycleimage" alt=""></img>
-      </div> */}
+      </div>
     </div>
   );
 }
