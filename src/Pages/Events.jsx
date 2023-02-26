d6ba75ba8607b9988419e4553d1f29d249e56f72
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-02-26 08:58:45 +0530

    Update Events.jsx

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
+
+// Components
 import Header from "../Components/Events/header/jsx/Header.jsx";
 import Card from "../Components/Events/card/jsx/Card.jsx";
 
 export default function Events() {
+  // scroll to top
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div>
       <Header head="Previous Events" />
