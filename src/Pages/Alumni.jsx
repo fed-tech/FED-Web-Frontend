468121ad4714ee3e50a14e619df54eb5bba5ac25
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-02-26 08:56:24 +0530

    Update Alumni.jsx

--- src/Pages/Alumni.jsx
+++ src/Pages/Alumni.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// Components
 import OurAlumni from "../Components/Alumni/OurAlumni";
+
 function Alumni() {
-  React.useEffect(() => {
+  useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
