7d06031476214b234dab0d650f959cd8aa92e0b6
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-03-03 00:54:34 +0530

    .

--- src/Components/Animatedpage.jsx
+++ src/Components/Animatedpage.jsx
@@ -6,27 +6,27 @@ const animations = {
   exit: { opacity: 0, x: -100 },
 };
 export const Animatedpage = ({ children }) => {
-  const variants = {
-    hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
-  };
+  // const variants = {
+  //   hidden: { opacity: 0, x: -200, y: 0 },
+  //   enter: { opacity: 1, x: 0, y: 0 },
+  //   exit: { opacity: 0, x: 0, y: -100 },
+  // };
   return (
     <motion.div
-      //variants={animations}
+      variants={animations}
       // initial={{ opacity: 0 }}
-      // animate={{ opacity: 1 }}
-      // exit={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
 
       // initial={{ opacity: 0, scale: 0.5 }}
       // animate={{ opacity: 1, scale: 1 }}
       // transition={{ duration: 0.5 }}
 
-      variants={variants} // Pass the variant object into Framer Motion
-      initial="hidden" // Set the initial state to variants.hidden
-      animate="enter" // Animated state to variants.enter
-      exit="exit" // Exit state (used later) to variants.exit
-      transition={{ type: "linear" }} // Set the transition to linear
+      // variants={variants} // Pass the variant object into Framer Motion
+      // initial="hidden" // Set the initial state to variants.hidden
+      // animate="enter" // Animated state to variants.enter
+      // exit="exit" // Exit state (used later) to variants.exit
+      // transition={{ type: "linear" }} // Set the transition to linear
     >
       {children}
     </motion.div>
