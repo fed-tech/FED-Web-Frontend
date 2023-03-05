dcd2c1b8fb80740ba577325eab506123922e6121
me-mystic <Shrutikolla.2002@gmail.com>
2023-03-05 13:28:01 +0530

    .

--- src/Components/Profile/MyProfile.jsx
+++ src/Components/Profile/MyProfile.jsx
@@ -5,25 +5,27 @@ export default function MyProfile() {
         <div>
             <section id="MyProfile">
                 <div className="container">
-                    <h3>FED</h3>
+                    <h3 className="logo-white">FED</h3>
                     <div className="signup-div">
                         <div className="signup-card">
-                            <h4>Hello there</h4>
-                            <h6>Please enter your details</h6>
-                            <br /><br />
-                            <div className="signup-with">
+                            <p className="whiteclass">Hello there</p>
+                            <p className="blackclass">Please enter your details</p>
+                            <div className="signup-with blackclass">
                                 Sigup with Google
                             </div>
                             <br />
-                            <p>or</p>
-                            <input type="text" placeholder="First Name"/>
-                            <hr />
-                            <input type="text" placeholder="Last Name"/>
-                            <hr />
-                            <input type="email" placeholder="Email"/>
-                            <hr />
-                            <input type="text" placeholder="Password"/>
-                            <hr />
+                            <p className="blackclass">or</p>
+                            <div className="input-padding">
+                                <input type="text" placeholder="First Name"/>
+                                <hr />
+                                <input type="text" placeholder="Last Name"/>
+                                <hr />
+                                <input type="email" placeholder="Email"/>
+                                <hr />
+                                <input type="text" placeholder="Password"/>
+                                <hr />
+                                <button type="submit" className="submit-btn"> Signup</button>
+                            </div>
                         </div>
                     </div>
                 </div>
