502ee1d8a1d34e9e296caf51d0ab550d69d369f8
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-18 20:52:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
 import Team from "./Pages/Team";
 import Error from "./Pages/Error";
+import Alumni from "./Pages/Alumni";
 import Events from "./Pages/Events";
 import Podcasts from "./Pages/Podcasts";
+import Seeall from "./Components/Home/Seeall";
 
 // Components
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
 import NavMobile from "./Components/NavMobile";
-import Alumni from "./Pages/Alumni";
-import Seeall from "./Components/Home/Seeall";
 
 function App() {
   return (
