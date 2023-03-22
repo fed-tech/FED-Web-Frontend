d5c9ea5e59f760b0f250d29fd22c9a67235139f9
me-mystic <Shrutikolla.2002@gmail.com>
2023-03-22 17:04:35 +0530

    .

--- src/Components/Profile/MyProfile.jsx
+++ src/Components/Profile/MyProfile.jsx
@@ -5,27 +5,16 @@ export default function MyProfile() {
         <div>
             <section id="MyProfile">
                 <div className="container">
-                    <h3 className="logo-white">FED</h3>
+                    <h3 id="logo-white">FED</h3>
                     <div className="signup-div">
                         <div className="signup-card">
                             <p className="whiteclass">Hello there</p>
                             <p className="blackclass">Please enter your details</p>
-                            <div className="signup-with blackclass">
+                            <p className="signup-with">
                                 Sigup with Google
-                            </div>
+                            </p>
                             <br />
-                            <p className="blackclass">or</p>
-                            <div className="input-padding">
-                                <input type="text" placeholder="First Name"/>
-                                <hr />
-                                <input type="text" placeholder="Last Name"/>
-                                <hr />
-                                <input type="email" placeholder="Email"/>
-                                <hr />
-                                <input type="text" placeholder="Password"/>
-                                <hr />
-                                <button type="submit" className="submit-btn"> Signup</button>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
