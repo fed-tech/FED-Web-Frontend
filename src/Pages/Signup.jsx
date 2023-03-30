14148d9964d7811a9836e95bc3679f90f63d52cf
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-30 10:22:50 +0530

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -11,6 +11,7 @@ export default function Signup() {
     <div className={SuCss.mDiv}>
       <div className={SuCss.glassDiv}>
         <p className={SuCss.FED}>FED</p>
+        <Form />
       </div>
     </div>
   );
