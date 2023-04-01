3885449be06c34e7e056f908102fd9f66ccfe8af
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-04-02 00:20:09 +0530

    .

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Css/Profilecss/profile.css";
-export default function Profile(){
-return(
+export default function Profile() {
+  return (
     <div className="profileBackground">
-        <div className="pmainBox">
-
+      <div className="pmainBox">
+        <div className="profileLeft"></div>
+        <div className="profileRight">
+          <div className="illuminating">ILLUMINATIING</div>
+          <p className="the">THE </p>
+          <p className="entrepreneur">ENTREPRENEUR</p>
+          <p className="inYou">IN YOU</p>
         </div>
+      </div>
     </div>
-
-);
+  );
 }
