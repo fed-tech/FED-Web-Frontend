3951590c350c600c60ef7185449cdc4de0b1f59c
me-mystic <Shrutikolla.2002@gmail.com>
2023-04-02 14:21:13 +0530

    .

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import "./Css/Profilecss/profile.css";
+import penSvg from "../Img/pen-icon.svg";
 export default function Profile() {
   return (
     <div className="profileBackground">
+      
       <div className="pmainBox">
-        <div className="profileLeft"></div>
+        <div className="profileLeft">
+          <div className="profile">
+            <div className="proHeading">
+              <p className="headInnerText">
+                <p>Profile Details</p> 
+                <img src={penSvg} alt="" />
+              </p> 
+            </div>
+            <p className="dets">Full Name</p>
+            <p className="dets">Roll Number</p>
+            <p className="dets">Email ID</p>
+            <p className="dets">Year</p>
+            <p className="dets">School</p>
+            <p className="dets">College</p>
+            <p className="dets">Mobile No</p>
+          </div>
+        </div>
         <div className="profileRight">
-          <div className="illuminating">ILLUMINATIING</div>
+          <p className="illuminating">ILLUMINATIING
+            <br />
+            THE 
+            <br />
+            <span>ENTREPRENEUR</span>
+            <br />
+            IN YOU
+          </p>
+          {/* <div className="illuminating">ILLUMINATIING</div>
           <p className="the">THE </p>
           <p className="entrepreneur">ENTREPRENEUR</p>
-          <p className="inYou">IN YOU</p>
+          <p className="inYou">IN YOU</p> */}
         </div>
       </div>
+
     </div>
   );
 }
