9dab1f4f1c2ae092d83999ffaf2d777e4362d095
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2023-04-11 23:44:02 +0530

    commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,57 +3,62 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../Pages/Css/loginpg.css";
 import google from "../Img/Google.svg";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Cookies, useCookies } from "react-cookie";
 import bcrypt from "bcryptjs-react";
 function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [passwrd, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [emailerr, setEmailerr] = useState(false);
+  const [passwrderr, setPasswrderr] = useState(false);
+  const [passwrd, setPassword] = useState("");
   const [isinValid, setIsinValid] = useState(false);
-  const [errmssg, setErrMssg] = useState('');
+  const [errmssg, setErrMssg] = useState("");
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   console.log(email);
   console.log(passwrd);
   useEffect(() => {
     setIsinValid(false);
-  }, [email,passwrd]);
-  const handlelogin=async(e)=>{
+    setEmailerr(false);
+    setPasswrderr(false);
+  }, [email, passwrd]);
+  const handlelogin = async (e) => {
     e.preventDefault();
     const username = email;
-    if(email === '' || passwrd === '')
-    {
-      setIsinValid(true);
-      setErrMssg('Please fill all the fields');
-      return
+    if (email === "") {
+      setEmailerr(true);
     }
+    if (passwrd === "") {
+      setPasswrderr(true);
+    }
+    else{
     try {
-      const password = bcrypt.hashSync(passwrd, '$2b$10$Q0RPeouqYdTToq76zoccIO');
-      console.log(password)
-      const response = await axios.post(`http://localhost:5000/auth/login`,{
+      const password = bcrypt.hashSync(
+        passwrd,
+        "$2b$10$Q0RPeouqYdTToq76zoccIO"
+      );
+      console.log(password);
+      const response = await axios.post(`http://localhost:5000/auth/login`, {
         username,
         password,
       });
       setCookie("AuthToken", response.data.user);
       // const success = response.status === 'ok';
       console.log(response.data);
-      if(response.status !== 202)
-      {
+      if (response.status !== 202) {
         setIsinValid(true);
-        setErrMssg("Invalid credentials")
+        setErrMssg("Invalid credentials");
+      } else {
+        navigate("/MyProfile");
       }
-      else{
-        navigate('/MyProfile');
-      }
-    }catch(err)
-    {
+    } catch (err) {
       setIsinValid(true);
-      setErrMssg("Invalid credentials")
+      setErrMssg("Invalid credentials");
       console.log(err);
     }
   }
+  };
 
-  
   return (
     <div className="full">
       <div className="inside">
@@ -76,6 +81,9 @@ function Login() {
               placeholder="Email"
               className="username"
               onChange={(e) => setEmail(e.target.value)}
+              style={{
+                borderBottom: emailerr ? "1px solid red" : "1px solid black",
+              }}
             />
           </div>
           <div className="pass">
@@ -84,17 +92,27 @@ function Login() {
               placeholder="Password"
               className="password"
               onChange={(e) => setPassword(e.target.value)}
+              style={{
+                borderBottom: passwrderr ? "1px solid red" : "1px solid black",
+              }}
             />
           </div>
-          <button className="logtwo" onClick={handlelogin}>Login</button>
+          <button className="logtwo" onClick={handlelogin}>
+            Login
+          </button>
           <div className="dont">
             <p className="signup">
               Don't have an account?{" "}
-              <Link to='/Signup'>
+              <Link to="/Signup">
                 <span className="spn">Signup</span>
               </Link>
             </p>
-            {isinValid && <p id="errmssg">{errmssg}</p>}
+            <p
+              id="errmssg"
+              style={{ visibility: isinValid ? "visible" : "hidden" }}
+            >
+              {errmssg}
+            </p>
           </div>
         </div>
       </div>
