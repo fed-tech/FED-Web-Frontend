7a0687cdd0da84bcafebec57f0974bbb091fb540
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-04-26 00:32:12 +0530

    .

--- src/Components/Home/LetsTalkBusiness.jsx
+++ src/Components/Home/LetsTalkBusiness.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/LetsTalkBusiness.css";
 
 export default function LetsTalkBusiness() {
+  const [formData, setFormData] = useState({});
+
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    const storedData = JSON.parse(localStorage.getItem("formData")) || [];
+    const newKey = Date.now().toString();
+    const newData = { ...storedData, [newKey]: formData };
+    localStorage.setItem("formData", JSON.stringify(newData));
+
+    const data = localStorage.getItem("formData");
+    console.log("Stored data:", data);
+    //localStorage.clear()
+    setFormData({});
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
   return (
     <div class="letsTalkBusiness" id="ContactUs">
       <div class="letsTalkBusiness-left">
@@ -19,19 +39,36 @@ export default function LetsTalkBusiness() {
         <form>
           <div>
             <label for="Name">Name:</label>
-            <input type="text" name="Name" />
+            <input
+              type="text"
+              name="Name"
+              id="Name"
+              required=""
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label for="Email">Email:</label>
-            <input type="email" name="Email" />
+            <input
+              type="email"
+              name="Email"
+              id="Email"
+              required=""
+              onChange={handleChange}
+            />
           </div>
           <div>
-            <label for="Message" id="message">
-              Message:
-            </label>
-            <textarea name="Message" cols="30" rows="10"></textarea>
+            <label for="Message">Message:</label>
+            <textarea
+              name="Message"
+              id="message"
+              cols="30"
+              rows="10"
+              required=""
+              onChange={handleChange}
+            ></textarea>
           </div>
-          <button type="submit" name="submit">
+          <button type="submit" name="submit" id="send" onClick={handleSubmit}>
             Send
           </button>
         </form>
