ec35a5630100104b279375dcb78ea73a129acc6c
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 18:02:44 +0530

    Update Loading.jsx

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+// Css
+import LCss from "./Css/Loading.module.css";
+
 export default function Loading() {
   return (
-    <div className="centerLoader">
-      <div className="arc" />
-      <h1 className="loadingSpanH1">
-        <span className="loadingSpan">Loading</span>
+    <div className={LCss.centerLoader}>
+      <div className={LCss.arc} />
+      <h1 className={LCss.loadingSpanH1}>
+        <span className={LCss.loadingSpan}>Loading</span>
       </h1>
     </div>
   );
