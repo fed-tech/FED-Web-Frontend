f3e255dc58d321782011bf4cfb1a78b0aa5038db
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 18:11:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ function App() {
           <Nav />
           <NavMobile />
           <Routes>
-            <Route path="/load" element={<Loading />}></Route>
             <Route
               path="/"
               element={
