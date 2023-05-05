c354dadfb1fe1e0349e776d9c99c1ebda8555737
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 17:59:21 +0530

    Update Loading.jsx

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="centerLoader">
+      <div className="arc" />
+      <h1 className="loadingSpanH1">
+        <span className="loadingSpan">Loading</span>
+      </h1>
+    </div>
+  );
 }
