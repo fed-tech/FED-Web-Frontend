a4d53a3aab3781fe9436d355f190590357d056ab
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-05 19:06:55 +0530

    Update NavMobile.jsx

--- src/Components/NavMobile.jsx
+++ src/Components/NavMobile.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
 import "./css/MobileNav.css";
 
 export default function NavMobile() {
