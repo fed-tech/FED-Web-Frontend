384a10fa504a75a9cd1015a4f34341d7c0db5db5
Antara-3011 <104826880+Antara-3011@users.noreply.github.com>
2023-05-21 17:37:58 +0530

    .

--- src/Components/NavMobile.jsx
+++ src/Components/NavMobile.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import "./css/MobileNav.css";
+// state
+import AuthContext from "./../store/auth-context";
 
-export default function NavMobile() {
+export default function NavMobile(props) {
   const [count, setCount] = useState(false);
   function toggleEvent() {
     setCount(true);
   }
+  const authCtx = useContext(AuthContext);
   return (
     <header>
       <nav className="mobileNav">
@@ -82,6 +85,23 @@ export default function NavMobile() {
               Contact Us
             </HashLink>
           </div> */}
+          <div class="mobileList">
+            <NavLink
+              to={authCtx.isLoggedIn ? "/MyProfile" : "Signup"}
+              className="liTag"
+            >
+              {authCtx.isLoggedIn ? (
+                <img
+                  src={authCtx.user.pic}
+                  alt=""
+                  srcset=""
+                  className="profile_img"
+                />
+              ) : (
+                "Login/SignUp"
+              )}
+            </NavLink>
+          </div>
         </div>
       </div>
     </header>
