dfb5e51fe9e63584372a474f83fbe8494b0db828
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-30 21:06:24 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
 
-// Helmet
-import { Helmet } from "react-helmet";
-
 // Components
 import Carousel from "../Components/Home/Carousel";
 import KnowUsCom from "../Components/Home/KnowUs";
@@ -24,39 +21,6 @@ export default function Home() {
 
   return (
     <>
-      <Helmet>
-        <title>Federation of Entrepreneurship Development</title>
-        <meta
-          name="description"
-          content="Our Homepage is the main webpage of a website that serves as the primary entry point for visitors. Its purpose is to provide an overview of what our society stands for, what we believe in and what others think about FED."
-        />
-        <meta
-          property="og:title"
-          content="Federation of Entrepreneurship Development"
-        />
-        <meta property="og:url" content="https://fedkiit.com" />
-        <meta
-          property="og:description"
-          content="Our Homepage is the main webpage of a website that serves as the primary entry point for visitors. Its purpose is to provide an overview of what our society stands for, what we believe in and what others think about FED. "
-        />
-        <meta
-          property="og:image"
-          itemprop="image"
-          content="https://ik.imagekit.io/vgd1bllwv/fed.png?updatedAt=1685457447183"
-        />
-        <meta property="og:type" content="website" />
-
-        <meta property="twitter:card" content="summary_large_image"></meta>
-        <meta
-          property="twitter:url"
-          content="https://twitter.com/services_enr"
-        ></meta>
-        <meta
-          property="twitter:title"
-          content="Federation of Entrepreneurship Development"
-        ></meta>
-      </Helmet>
-
       <Carousel />
       <KnowUsCom />
       <BelieveCom />
