9e7bbcc8b4ac99fbed7237cadabb0d7e3b7fd2a1
Rishav Singh <rishavrajcrj@gmail.com>
2023-05-30 21:06:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { Suspense } from "react";
 
+// Helmet
+import { Helmet } from "react-helmet";
+
 // Layout
 import Layout from "./Pages/Layout";
 
@@ -32,6 +35,42 @@ function App() {
           <Nav />
           <NavMobile />
           <Routes>
+            <Helmet>
+              <title>Federation of Entrepreneurship Development</title>
+              <meta
+                name="description"
+                content="Our Homepage is the main webpage of a website that serves as the primary entry point for visitors. Its purpose is to provide an overview of what our society stands for, what we believe in and what others think about FED."
+              />
+              <meta
+                property="og:title"
+                content="Federation of Entrepreneurship Development"
+              />
+              <meta property="og:url" content="https://fedkiit.com" />
+              <meta
+                property="og:description"
+                content="Our Homepage is the main webpage of a website that serves as the primary entry point for visitors. Its purpose is to provide an overview of what our society stands for, what we believe in and what others think about FED. "
+              />
+              <meta
+                property="og:image"
+                itemprop="image"
+                content="https://ik.imagekit.io/vgd1bllwv/fed.png?updatedAt=1685457447183"
+              />
+              <meta property="og:type" content="website" />
+
+              <meta
+                property="twitter:card"
+                content="summary_large_image"
+              ></meta>
+              <meta
+                property="twitter:url"
+                content="https://twitter.com/services_enr"
+              ></meta>
+              <meta
+                property="twitter:title"
+                content="Federation of Entrepreneurship Development"
+              ></meta>
+            </Helmet>
+
             <Route path="/" element={<Home />} />
             <Route
               path="/Team"
