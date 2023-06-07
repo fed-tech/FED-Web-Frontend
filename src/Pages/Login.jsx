1fd4c3f1b8cc47f62363b181ab426d28999f579d
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2023-06-07 23:49:02 +0530

    google login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -138,7 +138,7 @@ function Login(props) {
                       resp.data.result[0].College,
                       resp.data.result[0].MobileNo,
                       resp.data.result[0].selected,
-                      resp.data.result[0].access,
+                      Number(resp.data.result[0].access),
                       resp.data.token,
                       10800000
                     );
