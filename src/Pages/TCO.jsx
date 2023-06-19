ea488acb95a17cd2c6b815700bf3236c6e9c8e9a
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-19 14:34:16 +0530

    .

--- src/Pages/TCO.jsx
+++ src/Pages/TCO.jsx
@@ -176,22 +176,32 @@ export default function TCO() {
                       <th>Event Date</th>
                     </tr>
 
-                    <tr className="tableData">
+                    <tr className="tableData font1">
                       <td>KALKI</td>
                       <td>500</td>
                       <td>13 FEBRUARY</td>
                     </tr>
-                    <tr className="tableData">
+                    <tr className="tableData font1">
                       <td>KALKI</td>
                       <td>500</td>
                       <td>13 FEBRUARY</td>
                     </tr>
-                    <tr className="tableData">
+                    <tr className="tableData font1">
                       <td>KALKI</td>
                       <td>500</td>
                       <td>13 FEBRUARY</td>
                     </tr>
-                    <tr className="tableData">
+                    <tr className="tableData font1">
+                      <td>KALKI</td>
+                      <td>500</td>
+                      <td>13 FEBRUARY</td>
+                    </tr>
+                    <tr className="tableData font1">
+                      <td>KALKI</td>
+                      <td>500</td>
+                      <td>13 FEBRUARY</td>
+                    </tr>
+                    <tr className="tableData font1">
                       <td>KALKI</td>
                       <td>500</td>
                       <td>13 FEBRUARY</td>
@@ -256,7 +266,6 @@ export default function TCO() {
                       </div>
 
                     </div>
-
                     <div className="row">
 
                       <tr className="eTableData">
@@ -270,6 +279,7 @@ export default function TCO() {
                       </div>
 
                       <div className="media">
+                        <div className="mediaTop">
                         <div className="source">
                           <p>By Mail:</p>
                           <p>12345</p>
@@ -278,6 +288,9 @@ export default function TCO() {
                           <p>By Instagram:</p>
                           <p>12345</p>
                         </div>
+
+                        </div>
+                        <div className="mediaBottom">
                         <div className="source">
                           <p>By Whatsapp:</p>
                           <p>12345</p>
@@ -285,6 +298,84 @@ export default function TCO() {
                         <div className="source">
                           <p>By LinkedIn:</p>
                           <p>12345</p>
+                        </div>
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <div className="row">
+
+                      <tr className="eTableData">
+                        <td>KALKI</td>
+                        <td>13 FEBRUARY</td>
+                        <td>500</td>
+                      </tr>
+
+                      <div className="desc font1">
+                        Registration Stats
+                      </div>
+
+                      <div className="media">
+                        <div className="mediaTop">
+                        <div className="source">
+                          <p>By Mail:</p>
+                          <p>12345</p>
+                        </div>
+                        <div className="source">
+                          <p>By Instagram:</p>
+                          <p>12345</p>
+                        </div>
+
+                        </div>
+                        <div className="mediaBottom">
+                        <div className="source">
+                          <p>By Whatsapp:</p>
+                          <p>12345</p>
+                        </div>
+                        <div className="source">
+                          <p>By LinkedIn:</p>
+                          <p>12345</p>
+                        </div>
+
+                        </div>
+                      </div>
+
+                    </div>
+                    <div className="row">
+
+                      <tr className="eTableData">
+                        <td>KALKI</td>
+                        <td>13 FEBRUARY</td>
+                        <td>500</td>
+                      </tr>
+
+                      <div className="desc font1">
+                        Registration Stats
+                      </div>
+
+                      <div className="media">
+                        <div className="mediaTop">
+                        <div className="source">
+                          <p>By Mail:</p>
+                          <p>12345</p>
+                        </div>
+                        <div className="source">
+                          <p>By Instagram:</p>
+                          <p>12345</p>
+                        </div>
+
+                        </div>
+                        <div className="mediaBottom">
+                        <div className="source">
+                          <p>By Whatsapp:</p>
+                          <p>12345</p>
+                        </div>
+                        <div className="source">
+                          <p>By LinkedIn:</p>
+                          <p>12345</p>
+                        </div>
+
                         </div>
                       </div>
 
