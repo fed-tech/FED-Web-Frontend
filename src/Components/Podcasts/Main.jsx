f0de3e4784a13cc6374e1adb63013342b3ef5e41
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-25 16:34:39 +0530

    Update Main.jsx

--- src/Components/Podcasts/Main.jsx
+++ src/Components/Podcasts/Main.jsx
@@ -19,9 +19,6 @@ function Main(props) {
                 <p className="para1">
                   The <span className="para2">FedPreneur</span> Show.
                 </p>
-                <button className="cover-btn" onClick={newState}>
-                  Tune in Now
-                </button>
               </div>
               <p className="para3">
                 The FEDpreneur Show is our podcast series in which we host
@@ -54,10 +51,23 @@ function Main(props) {
                 <p className="para1">
                   The <span className="para2">FedPreneur</span> Show.
                 </p>
-                <button className="cover-btn" onClick={newState}>
-                  Tune in Now
-                </button>
               </div>
+              <p className="para3">
+                The FEDpreneur Show is our podcast series in which we host
+                India's most successful entrepreneurs and strive to uncover
+                their secrets to success, learning, and entertainment! Every
+                conversation on The FEDpreneur Show offers intellectual,
+                educational, and extreme learning opportunities to the audience.
+                It's about a visionary entrepreneur session with a group of
+                multi-talented and experienced business owners. It would aid our
+                young brains to stand strong in marketing and startup endeavors
+                with our podcasts at "The FEDpreneur Show," where we host
+                India's best success stories and aim to dig out their secrets to
+                success, learning, and enjoyment!
+              </p>
+              <button className="cover-btn" onClick={newState}>
+                Tune in Now
+              </button>
             </div>
           </section>
           <section id="goToYoutube" style={{ display: "none" }}>
