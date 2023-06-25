14d10bcf7a1928b472aa25f0bd3cff081ee73738
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-25 16:23:11 +0530

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -5,24 +5,21 @@ import "./Css/err.css";
 
 // assets
 import Errimage from "./../assets/Layer 3.svg";
-import { Animatedpage } from "../Components/Animatedpage";
 
 function Errorpage() {
   return (
-    <Animatedpage>
-      <div className="bg">
-        <div className="oops">Oops!</div>
-        <p className="notfound">Error 404 page not found</p>
-        <p className="the_page">
-          The page you requested could not be found.
-          <br />
-          We're working on it :)
-        </p>
-        <div className="cycle_image">
-          <img src={Errimage} className="cycleimage" alt=""></img>
-        </div>
+    <div className="bg">
+      <div className="oops">Oops!</div>
+      <p className="notfound">Error 404 page not found</p>
+      <p className="the_page">
+        The page you requested could not be found.
+        <br />
+        We're working on it :)
+      </p>
+      <div className="cycle_image">
+        <img src={Errimage} className="cycleimage" alt=""></img>
       </div>
-    </Animatedpage>
+    </div>
   );
 }
 export default Errorpage;
