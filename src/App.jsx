201572dfd05a05963699be1f7ada9a1241704cf8
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-25 20:55:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,6 @@ import CreateProfile from "./Pages/CreateProfile";
 import Page from "./Pages/Page";
 import PrivacyPolicies from "./Pages/PrivacyPolicies";
 import TermsAndConditions from "./Pages/TermsAndConditions";
-import Load from "./Components/Load";
 
 // Components
 import Nav from "./Components/Nav";
@@ -56,7 +55,6 @@ function App() {
           <div className="page">
             <div className="pageExt">
               <Routes>
-                <Route path="/Load" element={<Load />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/Team" element={<Team />} />
                 <Route path="/Event" element={<Events />} />
