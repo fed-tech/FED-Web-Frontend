661ebef53514b3aa0316dba52a9e13704e5df773
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-25 17:55:28 +0530

    Update Main.jsx

--- src/Components/Podcasts/Main.jsx
+++ src/Components/Podcasts/Main.jsx
@@ -16,42 +16,39 @@ function Main(props) {
 
   return (
     <>
-      <>
-        <section
-          id="podcasts"
-          style={goToYoutube ? { display: "none" } : { display: "block" }}
-        >
-          <div className="KnowUsmDiv">
-            <div className="coverTitle">
-              <p className="para1">
-                The <span className="para2">FedPreneur</span> Show.
-              </p>
-            </div>
-            <p className="para3">
-              The FEDpreneur Show is our podcast series in which we host India's
-              most successful entrepreneurs and strive to uncover their secrets
-              to success, learning, and entertainment! Every conversation on The
-              FEDpreneur Show offers intellectual, educational, and extreme
-              learning opportunities to the audience. It's about a visionary
-              entrepreneur session with a group of multi-talented and
-              experienced business owners. It would aid our young brains to
-              stand strong in marketing and startup endeavors with our podcasts
-              at "The FEDpreneur Show," where we host India's best success
-              stories and aim to dig out their secrets to success, learning, and
-              enjoyment!
+      <section
+        id="podcasts"
+        style={goToYoutube ? { display: "none" } : { display: "block" }}
+      >
+        <div className="KnowUsmDiv">
+          <div className="coverTitle">
+            <p className="para1">
+              The <span className="para2">FedPreneur</span> Show.
             </p>
-            <button className="cover-btn" onClick={newState}>
-              Tune in Now
-            </button>
           </div>
-        </section>
-        <section
-          id="goToYoutube"
-          style={goToYoutube ? { display: "block" } : { display: "none" }}
-        >
-          <Youtube />
-        </section>
-      </>
+          <p className="para3">
+            The FEDpreneur Show is our podcast series in which we host India's
+            most successful entrepreneurs and strive to uncover their secrets to
+            success, learning, and entertainment! Every conversation on The
+            FEDpreneur Show offers intellectual, educational, and extreme
+            learning opportunities to the audience. It's about a visionary
+            entrepreneur session with a group of multi-talented and experienced
+            business owners. It would aid our young brains to stand strong in
+            marketing and startup endeavors with our podcasts at "The FEDpreneur
+            Show," where we host India's best success stories and aim to dig out
+            their secrets to success, learning, and enjoyment!
+          </p>
+          <button className="cover-btn" onClick={newState}>
+            Tune in Now
+          </button>
+        </div>
+      </section>
+      <section
+        id="goToYoutube"
+        style={goToYoutube ? { display: "block" } : { display: "none" }}
+      >
+        <Youtube />
+      </section>
     </>
   );
 }
