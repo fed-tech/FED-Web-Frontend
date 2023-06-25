d128a023ca5e8b5fed41c72937fb7800e6ca8fcb
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-25 22:23:22 +0530

    Update EventCards.jsx

--- src/Components/Profile/EventCards.jsx
+++ src/Components/Profile/EventCards.jsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from 'react'
-import './cssp/EventCards.css'
-import axios from 'axios'
+import React, { useEffect } from "react";
 
-function EventCards({info}) {
+// axios
+import axios from "axios";
+
+// css
+import "./cssp/EventCards.css";
+
+function EventCards({ info }) {
   return (
-    <div className='eventcards'>
-        <div className='event_img'>
-            <img src="https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" />
-        </div>
-        <div className='event_content'>
-            <h2>{info.title}</h2>
-            <p>{info.date}</p>
-        </div>
-      
+    <div className="eventcards">
+      <div className="event_img">
+        <img
+          src="https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+        />
+      </div>
+      <div className="event_content">
+        <h2>{info.title}</h2>
+        <p>{info.date}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default EventCards
+export default EventCards;
