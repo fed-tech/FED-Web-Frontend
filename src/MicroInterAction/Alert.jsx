4720c65ebb6dae796b77b23701e467831c762745
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 17:19:16 +0530

    Update Alert.jsx

--- src/MicroInterAction/Alert.jsx
+++ src/MicroInterAction/Alert.jsx
@@ -3,34 +3,32 @@ import React, { useState } from "react";
 // css
 import "./Css/Alert.css";
 
-export const Alert = ({ variant }) => {
-  const [open, setOpen] = useState(true);
-  if (open)
-    return (
-      <div className="alert-mDiv">
+export const Alert = ({ variant, val }) => {
+  return (
+    <div className="alert-mDiv">
+      <div
+        className="alert-container"
+        style={{
+          background: variant.mainColor,
+          border: "0.1rem solid " + variant.secondaryColor,
+        }}
+      >
         <div
-          className="alert-container"
-          style={{
-            background: variant.mainColor,
-            border: "0.1rem solid " + variant.secondaryColor,
-          }}
+          className="symbol-container"
+          style={{ background: variant.secondaryColor }}
         >
-          <div
-            className="symbol-container"
-            style={{ background: variant.secondaryColor }}
-          >
-            <span class="material-symbols-outlined symbol colorIcon">
-              {variant.symbol}
-            </span>{" "}
-          </div>
-          <div className="description-container">
-            <span className="description-title">{variant.title}:</span>
-            <span className="description-text"> {variant.text}</span>
-          </div>
-          <a className="symbol-close-link" onClick={() => setOpen(false)}>
-            <span class="material-symbols-outlined  colorIcon">close</span>
-          </a>
+          <span class="material-symbols-outlined symbol colorIcon">
+            {variant.symbol}
+          </span>{" "}
         </div>
+        <div className="description-container">
+          <span className="description-title">{variant.title}:</span>
+          <span className="description-text"> {variant.text}</span>
+        </div>
+        <a className="symbol-close-link" onClick={() => val({ val: false })}>
+          <span class="material-symbols-outlined  colorIcon">close</span>
+        </a>
       </div>
-    );
+    </div>
+  );
 };
