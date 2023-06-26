170e3a2eac88d4e8ff442493dd4e523f3aa97ea8
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 19:49:28 +0530

    Create SignUp.jsx

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      SignUp Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+      quasi eos exercitationem aliquam molestias error eveniet blanditiis
+      tempore dignissimos distinctio perspiciatis recusandae veritatis assumenda
+      modi est necessitatibus mollitia consequuntur illo earum voluptatibus
+      natus delectus ad esse consectetur. Pariatur similique maxime illum.
+      Distinctio voluptatum autem maxime consequuntur obcaecati earum fugit
+      dolorum tempora placeat. Minus necessitatibus corporis nostrum commodi in
+      doloremque, dicta harum rem delectus maiores id consequuntur? In iste
+      dolorem provident asperiores, voluptatibus nesciunt, quae repellat est
+      alias at assumenda consequatur unde reiciendis ad odit maxime, saepe eius
+      impedit tempora adipisci aliquid quis mollitia quam? Officia blanditiis
+      veniam voluptas rem consectetur?
+    </div>
+  );
+}
+
+export default SignUp;
