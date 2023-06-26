222cc610118d65347928d2d3427a20d849a0983a
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 19:53:30 +0530

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -14,7 +14,16 @@ function SignUp() {
       dolorem provident asperiores, voluptatibus nesciunt, quae repellat est
       alias at assumenda consequatur unde reiciendis ad odit maxime, saepe eius
       impedit tempora adipisci aliquid quis mollitia quam? Officia blanditiis
-      veniam voluptas rem consectetur?
+      veniam voluptas rem consectetur? Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Labore, vitae quas fuga illo deserunt, iste doloremque
+      at non, minus cumque quam nulla? Autem facilis eum iusto, eligendi illum
+      illo optio praesentium ipsa debitis odit culpa, provident quibusdam libero
+      perferendis ducimus! Earum quidem quae cumque vitae rem. Dolore autem odit
+      omnis minus impedit quo voluptatum quis. Recusandae maiores alias eos
+      corporis voluptatem saepe quam, soluta ratione doloribus quod minima ex ut
+      repellendus, qui magni nostrum eius harum velit odit laudantium? Magnam
+      consequuntur quas nostrum nulla ipsa ratione aperiam tempore ut cum quos
+      quae voluptatem numquam expedita rerum debitis, dolorem alias eaque!
     </div>
   );
 }
