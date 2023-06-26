f931bd3472c55e5a1f30b5c7fc4d830afa7d02ae
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 14:56:29 +0530

    Update Domain.jsx

--- src/Components/Team/Domain.jsx
+++ src/Components/Team/Domain.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import ImageComponent from "../../Pages/ImageComponentTeam";
+import ImageComponent from "./ImageComponentTeam";
 
 export default function Domain(props) {
   const [showContent2, setshowContent2] = useState(true);
