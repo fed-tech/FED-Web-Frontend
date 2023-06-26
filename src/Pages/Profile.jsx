1d294c69d9406931d131dcd5112739ed6e7c21e4
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 19:52:15 +0530

    Create Profile.jsx

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      Profile Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Blanditiis dignissimos consequatur expedita dolores, corporis asperiores
+      voluptatum aliquam quae nesciunt itaque, deleniti adipisci, repellendus
+      enim! Itaque odio aliquam veniam magnam soluta magni nam quod maxime
+      consequatur repudiandae maiores voluptatum quo optio iusto eligendi
+      deleniti architecto neque quibusdam ad, atque consectetur est. Et quos
+      voluptatem reiciendis ullam laboriosam similique, temporibus id numquam
+      nobis cumque voluptatibus nihil quae facilis sint commodi sapiente error
+      incidunt corrupti quidem modi alias. Corporis consequatur eos ratione
+      quidem, quibusdam quae aliquam explicabo optio eveniet nostrum esse sunt
+      odit, numquam vero est ea nisi dolores quaerat velit et blanditiis.
+    </div>
+  );
+}
+
+export default Profile;
