789f90329ce38e734c6a244d0a86828bac41e890
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 17:08:26 +0530

    Update Alert.jsx

--- src/MicroInterAction/Alert.jsx
+++ src/MicroInterAction/Alert.jsx
@@ -7,28 +7,30 @@ export const Alert = ({ variant }) => {
   const [open, setOpen] = useState(true);
   if (open)
     return (
-      <div
-        className="alert-container"
-        style={{
-          background: variant.mainColor,
-          border: "0.1rem solid " + variant.secondaryColor,
-        }}
-      >
+      <div className="alert-mDiv">
         <div
-          className="symbol-container"
-          style={{ background: variant.secondaryColor }}
+          className="alert-container"
+          style={{
+            background: variant.mainColor,
+            border: "0.1rem solid " + variant.secondaryColor,
+          }}
         >
-          <span class="material-symbols-outlined symbol colorIcon">
-            {variant.symbol}
-          </span>{" "}
+          <div
+            className="symbol-container"
+            style={{ background: variant.secondaryColor }}
+          >
+            <span class="material-symbols-outlined symbol colorIcon">
+              {variant.symbol}
+            </span>{" "}
+          </div>
+          <div className="description-container">
+            <span className="description-title">{variant.title}:</span>
+            <span className="description-text"> {variant.text}</span>
+          </div>
+          <a className="symbol-close-link" onClick={() => setOpen(false)}>
+            <span class="material-symbols-outlined  colorIcon">close</span>
+          </a>
         </div>
-        <div className="description-container">
-          <span className="description-title">{variant.title}:</span>
-          <span className="description-text">{variant.text}</span>
-        </div>
-        <a className="symbol-close-link" onClick={() => setOpen(false)}>
-          <span class="material-symbols-outlined  colorIcon">close</span>
-        </a>
       </div>
     );
 };
