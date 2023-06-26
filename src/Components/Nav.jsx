cf1c115fd7f9a3a64abb93d145994ce21b619b98
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-26 19:06:21 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
+
 import "./css/DesktopNav.css";
 
 export default function Nav() {
@@ -36,11 +36,6 @@ export default function Nav() {
                   Our Team
                 </NavLink>
               </li>
-              {/* <li>
-                <HashLink to="/#ContactUs" className="liTag">
-                  Contact Us
-                </HashLink>
-              </li> */}
             </ul>
           </div>
         </div>
