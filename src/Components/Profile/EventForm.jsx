10ff79ba655f7a6c073383c07047d3d26f043d13
me-mystic <Shrutikolla.2002@gmail.com>
2023-06-27 23:15:15 +0530

    .

--- src/Components/Profile/EventForm.jsx
+++ src/Components/Profile/EventForm.jsx
@@ -4,22 +4,25 @@ import React from 'react';
 import formCss from "../Profile/cssp/EventForm.module.css";
 
 export default function Form() {
-    return(
+    return (
         <>
-        <h1>NEW FORM</h1>
-        <form action="">
-            <input type="text" placeholder='Form Title*'/>
-            <input type="text" placeholder='About Event*'/>
-            <input type="text" placeholder='Event Type*'/>
-            <input type="text" placeholder='Amount'/>
-            <input type="text" placeholder='Priority'/>
-            <div className={formCss.formBottom}>
-            <input type="text" placeholder='Field Name*'/>
-            <input type="text" placeholder='Field Type*'/>
-            <input type="text" placeholder='Field Type*'/>
-            </div>
+            <div className={formCss.head}>
+                <h1>NEW FORM</h1>
 
-        </form>
+            </div>
+            <form action="" className={formCss.formDiv}>
+                <input type="text" placeholder='Form Title*' />
+                <input type="text" placeholder='About Event*' />
+                <input type="text" placeholder='Event Type*' />
+                <input type="text" placeholder='Amount' />
+                <input type="text" placeholder='Priority' />
+                <div className={formCss.formBottom}>
+                    <input type="text" placeholder='Field Name*' />
+                    <input type="text" placeholder='Field Type*' />
+                    <input type="text" placeholder='Field Type*' />
+                </div>
+                <button className={formCss.saveBtn}>SAVE</button>
+            </form>
         </>
     );
 }
