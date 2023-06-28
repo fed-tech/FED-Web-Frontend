ae0b148ec162901f8c4ada94015ebae6b2303aa9
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-28 15:15:51 +0530

    Create LoginFun.jsx

---
+++ src/Components/Login/LoginFun.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import Lcss from './css/loginpg.module.css'
+
+function LoginFun() {
+  return (
+    <>
+      <div className={Lcss.full}>
+        <div className={Lcss.inside}>
+          <div className={Lcss.title1}>
+            <p className={Lcss.FED}>FED</p>
+          </div>
+          <div className={Lcss.whitebox}>
+            <div className={Lcss.hellopart}>
+              <p className={Lcss.welc}>Welcome Back</p>
+              <p className={Lcss.det}>Please Enter your details</p>
+            </div>
+            <>
+                <LoginForm/> 
+            </>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginFun;
