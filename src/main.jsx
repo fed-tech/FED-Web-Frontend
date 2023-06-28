4ca5b882a07d2a11455593228ac9b6b32b049641
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-28 15:34:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,10 @@ import { AuthContextProvider } from "./store/auth-context";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <AuthContextProvider>
-    <GoogleOAuthProvider clientId="606182598951-2jbnd8127rc2hoaoapfm3te6t6bl8p72.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </AuthContextProvider>
