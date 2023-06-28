f65ec84b16f770ca5dcedad9d88da8556ac1ad4e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-28 17:02:57 +0530

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import SignUpFun from "../Components/signup/SignUpFun";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
+  const [modal, setModal] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleModel = () => {
+    setModal(!modal);
+    navigate("/Login");
+  };
   return (
-    <div>
-      SignUp Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-      quasi eos exercitationem aliquam molestias error eveniet blanditiis
-      tempore dignissimos distinctio perspiciatis recusandae veritatis assumenda
-      modi est necessitatibus mollitia consequuntur illo earum voluptatibus
-      natus delectus ad esse consectetur. Pariatur similique maxime illum.
-      Distinctio voluptatum autem maxime consequuntur obcaecati earum fugit
-      dolorum tempora placeat. Minus necessitatibus corporis nostrum commodi in
-      doloremque, dicta harum rem delectus maiores id consequuntur? In iste
-      dolorem provident asperiores, voluptatibus nesciunt, quae repellat est
-      alias at assumenda consequatur unde reiciendis ad odit maxime, saepe eius
-      impedit tempora adipisci aliquid quis mollitia quam? Officia blanditiis
-      veniam voluptas rem consectetur? Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Labore, vitae quas fuga illo deserunt, iste doloremque
-      at non, minus cumque quam nulla? Autem facilis eum iusto, eligendi illum
-      illo optio praesentium ipsa debitis odit culpa, provident quibusdam libero
-      perferendis ducimus! Earum quidem quae cumque vitae rem. Dolore autem odit
-      omnis minus impedit quo voluptatum quis. Recusandae maiores alias eos
-      corporis voluptatem saepe quam, soluta ratione doloribus quod minima ex ut
-      repellendus, qui magni nostrum eius harum velit odit laudantium? Magnam
-      consequuntur quas nostrum nulla ipsa ratione aperiam tempore ut cum quos
-      quae voluptatem numquam expedita rerum debitis, dolorem alias eaque!
-    </div>
+    <>
+      <SignUpFun />
+      {modal && (
+        <div className={SuCss.modal}>
+          <div className={SuCss.modalcontent}>
+            <img src={tick}></img>
+            <h2>SignUp Successfully!</h2>
+            <h3>Please check your mail !!</h3>
+            <button className={SuCss.ok} onClick={toggleModel}>
+              OK
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
