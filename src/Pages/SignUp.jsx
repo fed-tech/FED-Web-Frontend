b7d18bf6ce26a41035e229b81fbddaa9bbab1f82
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-29 17:59:54 +0530

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+// Components
 import SignUpFun from "../Components/signup/SignUpFun";
 
 function SignUp() {
