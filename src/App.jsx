ac578330dc4e9db8db0f633cc55cc809abc4d10c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-30 00:19:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import React, { Suspense, useContext } from "react";
 
 // Layout
@@ -100,7 +100,7 @@ function App() {
                   }
                 />
 
-                {!authCtx.isLoggedIn && (
+                {!authCtx.isLoggedIn ? (
                   <Route
                     path="/Login"
                     element={
@@ -109,9 +109,11 @@ function App() {
                       </Suspense>
                     }
                   />
+                ) : (
+                  <Navigate to="/MyProfile" />
                 )}
 
-                {!authCtx.isLoggedIn && (
+                {!authCtx.isLoggedIn ? (
                   <Route
                     path="/Register"
                     element={
@@ -120,9 +122,11 @@ function App() {
                       </Suspense>
                     }
                   />
+                ) : (
+                  <Navigate to="/MyProfile" />
                 )}
 
-                {authCtx.isLoggedIn && (
+                {authCtx.isLoggedIn ? (
                   <Route
                     path="/MyProfile"
                     element={
@@ -131,6 +135,8 @@ function App() {
                       </Suspense>
                     }
                   />
+                ) : (
+                  <Navigate to="/Login" />
                 )}
 
                 <Route
