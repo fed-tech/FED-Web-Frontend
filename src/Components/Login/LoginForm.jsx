2e601960a4fe609f4b17ac1987b382e5630a24ad
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-29 16:04:33 +0530

    Update LoginForm.jsx

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useGoogleLogin } from "@react-oauth/google";
 
 // Components
+import GoogleLogin from "./GoogleLogin";
 import Load from "./../../MicroInterAction/Load";
 import { Alert } from "./../../MicroInterAction/Alert";
 
@@ -164,100 +165,9 @@ function LoginForm() {
     }
   };
 
-  const login = useGoogleLogin({
-    onSuccess: (response) => setCodeResponse(response),
-  });
-
-  const loginWithGoogle = async () => {
-    try {
-      setLoad(true);
-
-      const googleResponse = await axios.get(
-        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${codeResponse.access_token}`,
-        {
-          headers: {
-            Authorization: `Bearer ${codeResponse.access_token}`,
-            Accept: "application/json",
-          },
-        }
-      );
-
-      let data = {
-        email: googleResponse.data.email,
-      };
-
-      const response = await axios.post("/auth/googleverification", data);
-
-      console.log(response.data);
-
-      if (response.data.status === true) {
-        setLoad(false);
-
-        authCtx.login(
-          response.data.user.name,
-          response.data.user.email,
-          response.data.user.img,
-          response.data.user.RollNumber,
-          response.data.user.School,
-          response.data.user.College,
-          response.data.user.MobileNo,
-          response.data.user.selected,
-          Number(response.data.user.access),
-          response.data.token,
-          10800000
-        );
-
-        console.log("Login Done 💯💯💯💯💯");
-
-        if (authCtx.target == null) {
-          navigate("/MyProfile");
-        } else {
-          navigate(`/${authCtx.target}`);
-          authCtx.settarget(null);
-        }
-
-        return;
-      } else {
-        setLoad(false);
-
-        if (response.data.code === 4) {
-          setError({
-            mainColor: "#E5F6FD",
-            secondaryColor: "#1AB1F5",
-            symbol: "info",
-            title: "Information",
-            text: "Email not verified",
-            val: true,
-          });
-        } else if (response.data.code === 2) {
-          setError({
-            mainColor: "#FFC0CB",
-            secondaryColor: "#FF69B4",
-            symbol: "pets",
-            title: "Check it out",
-            text: "Please Sign Up first",
-            val: true,
-          });
-        }
-      }
-    } catch (err) {
-      setLoad(false);
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    if (codeResponse) {
-      loginWithGoogle();
-    }
-  }, [codeResponse]);
-
   return (
     <>
-      <div className={Lcss.googlepart} onClick={() => login()}>
-        <img src={google} className={Lcss.icon}></img>
-        <p className={Lcss.log}>Login with Google</p>
-      </div>
+      <GoogleLogin />
       <p className={Lcss.or}>Or</p>
 
       <form className={Lcss.formTag}>
