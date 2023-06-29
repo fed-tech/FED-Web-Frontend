a88dced88233795e58f6b2b49d13a7f5e3e703f0
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-29 16:02:15 +0530

    Update GoogleLogin.jsx

---
+++ src/Components/Login/GoogleLogin.jsx
@@ -0,0 +1,9 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+// Oauth
+import { useGoogleLogin } from "@react-oauth/google";
+
+export default function GoogleLogin() {
+  return <div>GoogleLogin</div>;
+}
