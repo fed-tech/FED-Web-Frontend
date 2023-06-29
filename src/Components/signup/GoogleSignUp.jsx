7a36545862f2f4de9c154c843076a1ac27e98ba7
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-29 23:46:48 +0530

    Update GoogleSignUp.jsx

--- src/Components/signup/GoogleSignUp.jsx
+++ src/Components/signup/GoogleSignUp.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// Components
+import { Alert } from "./../../MicroInterAction/Alert";
+
+//  axios
+import axios from "axios";
+
+// state
+import AuthContext from "../../store/auth-context";
+
+// css
+import SuCss from "./css/Signup.module.css";
+
+// img
+import google from "./../../assets/Login/Google.svg";
+
 const GoogleSignUp = ({ setLoad }) => {
-  return <div>GoogleSignUp</div>;
+  return (
+    <>
+      <div className={SuCss.googleDiv} onClick={() => login()}>
+        <img src={google} className="icon"></img>
+        <p className={SuCss.googleText}>SignUp with google</p>
+      </div>
+
+      <Alert variant={variants} val={setError} />
+    </>
+  );
 };
 
 GoogleSignUp.propTypes = {
