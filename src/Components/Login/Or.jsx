a0427788aa97e7e1f72f4fe41109b5a757044db6
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-29 19:49:46 +0530

    Update Or.jsx

--- src/Components/Login/Or.jsx
+++ src/Components/Login/Or.jsx
@@ -6,9 +6,9 @@ import Lcss from "./css/loginpg.module.css";
 export default function Or() {
   return (
     <div className={Lcss.orMDIv}>
-      <div className={Lcss.orLine1}></div>
+      <div className={Lcss.orLine}></div>
       <p className={Lcss.or}>Or</p>
-      <div className={Lcss.orLine2}></div>
+      <div className={Lcss.orLine}></div>
     </div>
   );
 }
