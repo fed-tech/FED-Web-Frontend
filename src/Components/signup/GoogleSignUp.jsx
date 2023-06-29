14e87c5ccf41145cffdef227403c8522d3d3ddbf
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-29 23:43:24 +0530

    Update GoogleSignUp.jsx

---
+++ src/Components/signup/GoogleSignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GoogleSignUp = ({ setLoad }) => {
+  return <div>GoogleSignUp</div>;
+};
+
+GoogleSignUp.propTypes = {
+  setLoad: PropTypes.func.isRequired,
+};
+
+export default GoogleSignUp;
