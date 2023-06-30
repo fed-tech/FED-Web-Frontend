a20dbc7a05f54bea3071277b64f2f159dbdd7a12
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-01 00:49:01 +0530

    Update CompleteProfile.jsx

---
+++ src/Components/signup/CompleteProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function CompleteProfile(props) {
+  console.log(props);
+  return <div>CompleteProfile</div>;
+}
+
+CompleteProfile.propTypes = {};
+
+export default CompleteProfile;
