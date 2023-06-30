d1e8bd3453b70798944cbaa5de5f7712bfe4502b
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-30 11:45:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,13 @@ import App from "./App";
 // Google Auth
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-// Provider
-import { AuthContextProvider } from "./store/auth-context";
-
 // Css
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <AuthContextProvider>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </AuthContextProvider>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
 );
