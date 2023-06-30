bdd454d8745256fa0af38f236ca5d011b13c70a4
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-01 02:45:12 +0530

    Update CompleteProfile.jsx

--- src/Components/signup/CompleteProfile.jsx
+++ src/Components/signup/CompleteProfile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 // css
@@ -9,10 +9,20 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function CompleteProfile(props) {
   console.log(props);
+
+  // useEffect(() => {
+  //   console.log();
+  // }, [props.data]);
   return (
-    <div className={CPCss.mDiv}>
+    <div
+      className={CPCss.mDiv}
+      id={Object.keys(props.data).length > 0 ? "showCreate" : "hideCreate"}
+    >
       <div className={CPCss.mDivCon}>
-        <ArrowBackIcon className={CPCss.ArrowBackIcon} />
+        <ArrowBackIcon
+          className={CPCss.ArrowBackIcon}
+          onClick={() => props.set(false)}
+        />
         <div>
           <p className={CPCss.CreateProfile}>Create Profile</p>
           <p className={CPCss.Please}>Please enter Your Details</p>
