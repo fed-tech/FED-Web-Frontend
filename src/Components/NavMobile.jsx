2f39e9ac4a11f1d975eaed32b761f7095015b96e
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-07-02 15:02:26 +0530

    mobile nav

--- src/Components/NavMobile.jsx
+++ src/Components/NavMobile.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
-
-// css
 import "./css/MobileNav.css";
-
 // state
 import AuthContext from "./../store/auth-context";
 
-export default function NavMobile() {
+export default function NavMobile(props) {
   const [count, setCount] = useState(false);
 
+  function toggleEvent() {
+    setCount(true);
+  }
   const authCtx = useContext(AuthContext);
-
   return (
     <header>
       <nav className="mobileNav">
@@ -22,18 +21,13 @@ export default function NavMobile() {
                 <p className="Cross">X</p>
               </div>
             ) : (
-              <div
-                className="hamburger"
-                id="ham"
-                onClick={() => setCount(true)}
-              >
+              <div className="hamburger" id="ham" onClick={toggleEvent}>
                 <div id="bur1"></div>
                 <div id="bur2"></div>
                 <div id="bur3"></div>
               </div>
             )}
           </div>
-
           <Link to="/" className="LinkStyle" onClick={() => setCount(false)}>
             <div className="LogoDiv">
               <img src="https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/62eeaa9927e6aea4ff13590e_FedLogo.png"></img>
@@ -43,34 +37,18 @@ export default function NavMobile() {
           <div id="Navdarkright"></div>
         </div>
       </nav>
-
       <div className="mobileNavClass" id={count ? "mobileNavList" : "blank"}>
-        <div class="mobileList">
-          {authCtx.isLoggedIn && (
-            <NavLink to="/MyProfile" className="liTag">
-              <img
-                src={authCtx.user.pic}
-                alt=""
-                srcset=""
-                className="profile_img"
-              />
-            </NavLink>
-          )}
-        </div>
-
         <div class="mobileNavListChild">
           <div class="mobileList">
             <NavLink to="/" className="liTag" onClick={() => setCount(false)}>
               Home
             </NavLink>
           </div>
-
           <div class="mobileList" onClick={() => setCount(false)}>
             <NavLink to="/event" className="liTag">
               Events
             </NavLink>
           </div>
-
           <div class="mobileList">
             <NavLink
               to="/Podcasts"
@@ -80,7 +58,6 @@ export default function NavMobile() {
               Podcasts
             </NavLink>
           </div>
-
           <div class="mobileList">
             <NavLink
               to="/Team"
@@ -90,13 +67,46 @@ export default function NavMobile() {
               Our Team
             </NavLink>
           </div>
-
+          {/* <div className="mobileList">
+            <NavLink
+              to="/Alumni"
+              className="liTag"
+              onClick={() => setCount(false)}
+            >
+              Alumni
+            </NavLink>
+          </div> */}
+          {/* <div class="mobileList">
+            <HashLink
+              to="/#ContactUs"
+              className="liTag"
+              onClick={() => setCount(false)}
+            >
+              Contact Us
+            </HashLink>
+          </div> */}
           <div class="mobileList">
-            {!authCtx.isLoggedIn && (
-              <NavLink to="/Login" className="liTag">
-                Login/SignUp
-              </NavLink>
-            )}
+            <NavLink
+              // to={authCtx.isLoggedIn ? "/MyProfile" : "Signup"}
+              to={authCtx.isLoggedIn ? "/MyProfile/member" : "Signup"}
+              className="liTag"
+            >
+              {authCtx.isLoggedIn ? (
+                <img
+                  src={authCtx.user.pic}
+                  alt=""
+                  srcset=""
+                  className="profile_img"
+                />
+              ) : (
+                "Login/SignUp"
+              )}
+            </NavLink>
+          </div>
+          <div class="mobileList">
+            <NavLink to="/Alumni" className="liTag">
+              Alumni
+            </NavLink>
           </div>
         </div>
       </div>
