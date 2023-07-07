dc825b0cdbae9f06379eeb1f619164a5a3132918
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-07 15:07:25 +0530

    Update ForgotPassword.jsx

--- src/Pages/ForgotPassword.jsx
+++ src/Pages/ForgotPassword.jsx
@@ -7,12 +7,22 @@ import "./Css/forgotpass.css";
 // axios
 import axios from "axios";
 
+// Components
+import { Alert } from "./../MicroInterAction/Alert";
+
 function ForgotPassword() {
   const [otp, setOtp] = useState("");
   const [email, setEmail] = useState("");
   const [err, setErr] = useState("Invalid");
   const [showBtn, setShowBtn] = useState(false);
-
+  const [variants, setError] = useState({
+    mainColor: "",
+    secondaryColor: "",
+    symbol: "",
+    title: "",
+    text: "",
+    val: false,
+  });
   const navigate = useNavigate();
 
   const handleSendOtp = async (e) => {
@@ -102,50 +112,54 @@ function ForgotPassword() {
   }, [email, otp]);
 
   return (
-    <div className="full-page">
-      <div className="insidebox">
-        <div className="title2">
-          <p className="FED">FED</p>
-        </div>
-        <div className="whitebox1">
-          <div className="hellopart1">
-            <p className="welc1">Forgot Password</p>
+    <>
+      <div className="full-page">
+        <div className="insidebox">
+          <div className="title2">
+            <p className="FED">FED</p>
           </div>
-          <div className="user">
-            <input
-              type="text"
-              placeholder="Email"
-              className="username1"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {showBtn && (
+          <div className="whitebox1">
+            <div className="hellopart1">
+              <p className="welc1">Forgot Password</p>
+            </div>
+            <div className="user">
               <input
-                type="password"
-                placeholder="Enter OTP"
+                type="text"
+                placeholder="Email"
                 className="username1"
-                onChange={(e) => setOtp(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
               />
+              {showBtn && (
+                <input
+                  type="password"
+                  placeholder="Enter OTP"
+                  className="username1"
+                  onChange={(e) => setOtp(e.target.value)}
+                />
+              )}
+            </div>
+            {!showBtn && (
+              <button className="logtwo1" onClick={handleSendOtp}>
+                Send OTP
+              </button>
             )}
+            {showBtn && (
+              <button className="logtwo1" onClick={handleConfirmOtp}>
+                Verify OTP
+              </button>
+            )}
+            <p
+              id="errmssg1"
+              style={{ color: err !== "Invalid" ? "red" : "white" }}
+            >
+              {err}
+            </p>
           </div>
-          {!showBtn && (
-            <button className="logtwo1" onClick={handleSendOtp}>
-              Send OTP
-            </button>
-          )}
-          {showBtn && (
-            <button className="logtwo1" onClick={handleConfirmOtp}>
-              Verify OTP
-            </button>
-          )}
-          <p
-            id="errmssg1"
-            style={{ color: err !== "Invalid" ? "red" : "white" }}
-          >
-            {err}
-          </p>
         </div>
       </div>
-    </div>
+
+      <Alert variant={variants} val={setError} />
+    </>
   );
 }
 
