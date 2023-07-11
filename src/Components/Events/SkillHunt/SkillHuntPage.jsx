87e541a666ec6f2f7418c6f4bca779a893894331
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-11 10:28:17 +0530

    Create SkillHuntPage.jsx

---
+++ src/Components/Events/SkillHunt/SkillHuntPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+// Components
+import PopUpModal from "../../skillhunt/PopUpModal";
+import regImg from "../assets/SkillHunt/point.png";
+import SuccessModal from "../../skillhunt/SuccessModal";
+
+// css
+import "./Css/SkillHunt.css";
+
+function SkillHuntPage() {
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [showSuccess, setSuccess] = useState(false);
+  const handlePopUp = (e) => {
+    setShowPopUp(true);
+  };
+  return (
+    <div className="SkillHuntPage">
+      <section id="omega">
+        <div className="mainContainer"></div>
+        <div class="btnDivReg">
+          <button class="box-type" onClick={handlePopUp}>
+            <h2 class="reg">Register Now</h2>
+            {/* <!-- <h2 class="reg">Coming Soon</h2> --> */}
+            <img src={regImg} alt="" srcset="" id="point" />
+          </button>
+        </div>
+      </section>
+
+      {/* <!-- alert --> */}
+      {showSuccess && <SuccessModal setSuccess={setSuccess} />}
+      {showPopUp && (
+        <PopUpModal setShowPopUp={setShowPopUp} setSuccess={setSuccess} />
+      )}
+    </div>
+  );
+}
+
+export default SkillHuntPage;
