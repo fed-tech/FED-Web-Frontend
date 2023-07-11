498223fe33d7a97b542d8625ff2b81328a9ad17a
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-07-11 12:01:22 +0530

    skill Hunt event page

--- src/Components/Events/SkillHunt/SkillHunt.jsx
+++ src/Components/Events/SkillHunt/SkillHunt.jsx
@@ -15,24 +15,39 @@ export default function SkillHunt() {
       <div className={SHCss.ContDiv}>
         <div className={SHCss.leftDiv}>
           <img src={skillhuntlogo} alt="" className={SHCss.ImgTagSkillTag} />
-          <p>
-            “Talent you have naturally. Skill is only developed by hours and
-            hours and hours of beating on your craft” ~Will Smith
-            <br />
-            We are thrilled to invite you to be a part of the highly anticipated
-            Skill Hunt event organized by FED at KIIT.
-            <br />
-            Skill Hunt is where individuals like you, with unparalleled
-            expertise, come together to showcase their remarkable skills.
-            Whether you excel in design, programming, marketing, or any other
-            distinctive skill, we want to see what you've got!
-            <br />
-            We look forward to reviewing your exceptional portfolio. Don't miss
-            this chance to connect, receive feedback, and collaborate! Join
-            Skill Hunt now and let your talent shine!
-          </p>
           <div>
             <RegBtn />
+          </div>
+        </div>
+        <div className={SHCss.parentRight}>
+          <div className={SHCss.rightDiv}>
+            <p>
+              <i>
+                <b>
+                  “Talent you have naturally. Skill is only developed by hours
+                  and hours and hours of beating on your craft” ~Will Smith
+                </b>
+              </i>
+              <br />
+              <br />
+              We are thrilled to invite you to be a part of the highly
+              anticipated <b>Skill Hunt</b> event organized by <b>FED</b> at
+              KIIT.
+              <br />
+              <br />
+              Skill Hunt is where individuals like you, with unparalleled
+              expertise, come together to showcase their remarkable skills.
+              Whether you excel in{" "}
+              <b>
+                design, programming, marketing, or any other distinctive skill
+              </b>
+              , we want to see what you've got!
+              <br />
+              <br />
+              We look forward to reviewing your exceptional portfolio. Don't
+              miss this chance to connect, receive feedback, and collaborate!
+              Join Skill Hunt now and let your talent shine!
+            </p>
           </div>
         </div>
       </div>
