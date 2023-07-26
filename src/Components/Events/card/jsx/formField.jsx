384e5dc5cf5de366b7f06ca5e90de687775f2c35
me-mystic <Shrutikolla.2002@gmail.com>
2023-07-26 23:31:14 +0530

    .

--- src/Components/Events/card/jsx/formField.jsx
+++ src/Components/Events/card/jsx/formField.jsx
@@ -6,11 +6,11 @@ export default function formField({ ...field }) {
             {field.type === "radio" || field.type === "checkbox"?
                 <div className="inpField">
                     <label>{field.title}</label>
-                    <div className="input">
-                        {field.value.map(radio => <>
+                    <div className="input radioDiv">
+                        {field.value.map(radio => <div>
                             <input className="radioInp" type={field.type} value={radio} placeholder={field.placeholder} />
                             <label className='radioLabel'>{radio}</label>
-                        </>
+                        </div>
                         )}
                     </div>
                 </div> 
