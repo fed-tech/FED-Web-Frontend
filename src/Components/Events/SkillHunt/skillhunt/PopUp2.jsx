42879f48079c278acddcf31e8189c420a9712e61
Rishav Singh <rishavrajcrj@gmail.com>
2023-07-29 17:17:35 +0530

    Update PopUp2.jsx

--- src/Components/Events/SkillHunt/skillhunt/PopUp2.jsx
+++ src/Components/Events/SkillHunt/skillhunt/PopUp2.jsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QR from "./QR";
 
 function PopUp2({ dataInp, info }) {
+  const [QrImg, setQrImg] = useState(0);
+
+  useEffect(() => {
+    const amtQR = Object.values(info.workshops).filter((val) => {
+      return val === true;
+    }).length;
+
+    setQrImg(amtQR);
+  }, []);
   return (
     <>
       <div className="cardeve" id="div1_2" data-step-1>
@@ -10,11 +19,10 @@ function PopUp2({ dataInp, info }) {
           preferred workshop then the allotment or cancellation of allotment of
           learning workshops will be at the sole discretion of FED.
         </h3>
-        {info.speaker === "true" && (
-          <QR qrLink="https://uploads-ssl.webflow.com/63a4333d6709521275441c77/64c2cfe0bb7f8dae8d14746d_Picture2.png" />
-        )}
-        {info.speaker === "false" && (
+        {QrImg === 1 ? (
           <QR qrLink="https://uploads-ssl.webflow.com/63a4333d6709521275441c77/64c2ceaf78167dd32ed57513_Picture1.png" />
+        ) : (
+          <QR qrLink="https://uploads-ssl.webflow.com/63a4333d6709521275441c77/64c2cfe0bb7f8dae8d14746d_Picture2.png" />
         )}
         <h3 className="step-title h3marginTop">
           Last 4 digits of the Transaction ID
