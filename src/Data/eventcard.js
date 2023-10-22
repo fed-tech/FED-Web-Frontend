07f27f3fdf9d1fc87beac4a6eb3b956f4bf15921
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 16:32:55 +0530

    Update eventcard.js

--- src/Data/eventcard.js
+++ src/Data/eventcard.js
@@ -1,8 +1,8 @@
 export const eventcard = [
   {
     class: "skillHunt",
-    date: "29",
-    month: "January",
+    date: "06",
+    month: "August",
     img: "https://uploads-ssl.webflow.com/629d87f593841156e4e0d9a4/64ad21753343f7de91db6213_skillhuntlogo.png",
     title: "SkillHunt",
     dis: {
