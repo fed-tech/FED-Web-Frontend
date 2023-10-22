924189f0ff26f8c7e18f4e3e0ac8ef91ffc956ab
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-22 16:24:12 +0530

    Update Events.jsx

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -15,7 +15,7 @@ export default function Events() {
     <>
       <div>
         <Header head="Current Events" />
-        <SkillHunt />
+        {/* <SkillHunt /> */}
       </div>
       <div>
         <Header head="Previous Events" />
