5439284868757cbaaa8a8ea1cf3fa99966b7e223
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-26 11:46:33 +0530

    Update MyEvent.jsx

--- src/Components/Profile/Dashboard/MyEvent/MyEvent.jsx
+++ src/Components/Profile/Dashboard/MyEvent/MyEvent.jsx
@@ -1,91 +1,96 @@
-import { useEffect, useState,  useContext } from 'react'
-import MyEventCards from './MyEventCards'
-import "./MyEvent.css"
-import cancel from '../../../../assets/SkillHunt/XCircle.png';
-import axios from 'axios';
+import { useEffect, useState, useContext } from "react";
+import MyEventCards from "./MyEventCards";
+import MeCss from "./Css/MyEvent.module.css";
+import cancel from "../../../../assets/SkillHunt/XCircle.png";
+import axios from "axios";
 import AuthContext from "../../../../store/auth-context";
-import Load from '../../../../MicroInterAction/Load';
+import Load from "../../../../MicroInterAction/Load";
 export default function MyEvents() {
-  
-    const authCtx = useContext(AuthContext);
-    const [card,setCard] = useState([])
-    const [show,setShow] = useState(false)
-    const [cardNo,setCardNo] = useState("")
-    const [mainLoading,setMainLoading] = useState(true)
-    const [currTeam,setCurrTeam] = useState([])
-    const [teamLoading,setTeamLoading] = useState(true)
-    const getuserformdetails = async () => {
-      var result = await axios.get("/form/getuserformdetails", {
-        headers: {
-          Authorization: authCtx.token,
-        },
-      });
-      setMainLoading(false)
-      setCard(result.data)
-      console.log(result.data)
-    }
-    const getTeamDetails = async (info) =>{
-      setTeamLoading(true)
-      setCurrTeam([])
-      var result = await axios.get(`/form/getteamdetails?formid=${info._id}`,{
-        headers:{
-          Authorization:authCtx.token
-        }
-      })
-      setCurrTeam(result.data)
-      setTeamLoading(false)
-    }
+  const authCtx = useContext(AuthContext);
+  const [card, setCard] = useState([]);
+  const [show, setShow] = useState(false);
+  const [cardNo, setCardNo] = useState("");
+  const [mainLoading, setMainLoading] = useState(true);
+  const [currTeam, setCurrTeam] = useState([]);
+  const [teamLoading, setTeamLoading] = useState(true);
+  const getuserformdetails = async () => {
+    var result = await axios.get("/form/getuserformdetails", {
+      headers: {
+        Authorization: authCtx.token,
+      },
+    });
+    setMainLoading(false);
+    setCard(result.data);
+    console.log(result.data);
+  };
+  const getTeamDetails = async (info) => {
+    setTeamLoading(true);
+    setCurrTeam([]);
+    var result = await axios.get(`/form/getteamdetails?formid=${info._id}`, {
+      headers: {
+        Authorization: authCtx.token,
+      },
+    });
+    setCurrTeam(result.data);
+    setTeamLoading(false);
+  };
 
-    useEffect(()=>{
-      getuserformdetails()
-    },[])
-    currTeam.map((e)=>{
-      console.log(e)
-    })
-    return (
-      <div className="viewEventss">
-        <div className="viewevents">
-          {mainLoading ? <Load />:<></>}
-          {card.map((e,idx)=>{
-              return <MyEventCards
+  useEffect(() => {
+    getuserformdetails();
+  }, []);
+  currTeam.map((e) => {
+    console.log(e);
+  });
+  return (
+    <div className="viewEventss">
+      <div className="viewevents">
+        {mainLoading ? <Load /> : <></>}
+        {card.map((e, idx) => {
+          return (
+            <MyEventCards
               info={e}
               key={idx}
               setShow={setShow}
               setCardNo={setCardNo}
               getTeamDetails={getTeamDetails}
             />
-          })}
-        </div>
-        {show ? (
-            <div className='modal'>
-              {teamLoading ? <Load />:<></>}
-              <img
-                src={cancel}
-                alt=""
-                onClick={() => setShow(false)}
-                id="CloseIcon"
-              />
-              <table className='table'>
-                <th className="th" colSpan={2} style={{fontSize:"1.5rem"}}>Team Details</th>
-                <tr className='tr'>
-                  <th className='th'>Member</th>
-                  <th className='th'>Actions</th>
-                </tr>
-                {currTeam.map((member)=>{
-                  return <tr className="tr">
-                    <td className="td">{member.name}</td>
-                    <td className="td">
-                    <a href={`${member.token}`} target='_blank'>
+          );
+        })}
+      </div>
+      {show ? (
+        <div className="modal">
+          {teamLoading ? <Load /> : <></>}
+          <img
+            src={cancel}
+            alt=""
+            onClick={() => setShow(false)}
+            id="CloseIcon"
+          />
+          <table className="table">
+            <th className="th" colSpan={2} style={{ fontSize: "1.5rem" }}>
+              Team Details
+            </th>
+            <tr className="tr">
+              <th className="th">Member</th>
+              <th className="th">Actions</th>
+            </tr>
+            {currTeam.map((member) => {
+              return (
+                <tr className="tr">
+                  <td className="td">{member.name}</td>
+                  <td className="td">
+                    <a href={`${member.token}`} target="_blank">
                       <button className="deleteMemberBtn">Remove</button>
                     </a>
-                    </td>
-                  </tr>;
-                })}
-              </table>
-            </div>
-        ) : (
-          <></>
-        )}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
         </div>
-    );
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
