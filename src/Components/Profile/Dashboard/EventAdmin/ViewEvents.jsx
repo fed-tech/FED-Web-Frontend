a69233d7f5cfeb2b8dae5515f1c1c2a89910598f
Rishav Singh <rishavrajcrj@gmail.com>
2023-10-26 12:05:25 +0530

    Update ViewEvents.jsx

--- src/Components/Profile/Dashboard/EventAdmin/ViewEvents.jsx
+++ src/Components/Profile/Dashboard/EventAdmin/ViewEvents.jsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import EventCards from '../../EventCards'
-import '../../../css/Profile/Dashboard/EventAdmin/ViewEvents.css'
-import axios from 'axios';
-import EventDetails from '../../EventDetails';
+import React, { useEffect, useState } from "react";
+import EventCards from "../../EventCards";
+
+// css
+import "../../../css/Profile/Dashboard/EventAdmin/ViewEvents.css";
+
+// Componentss
+import EventDetails from "../../EventDetails";
+
+//  axios
+import axios from "axios";
 
 function ViewEvents({ showEvent, setShow, setCardNo, cardNo }) {
   const [events, setEvents] = useState([]);
@@ -13,24 +19,41 @@ function ViewEvents({ showEvent, setShow, setCardNo, cardNo }) {
     } else {
       console.log("Did not recieve events");
     }
-  }
+  };
+
   useEffect(() => {
     loadEvents();
   }, [showEvent]);
+
   return (
-    <div className='viewEventss'>
-        {showEvent?
+    <div className="viewEventss">
+      {showEvent ? (
         <div className="viewEventDets">
-          <EventDetails cardNo = {cardNo} setShow = {setShow}/>
+          <EventDetails cardNo={cardNo} setShow={setShow} />
         </div>
-        :
+      ) : (
         <div className="viewevents">
-          {events.length==0?<><div style={{marginLeft:"20%"}}><h2>No Events Found</h2></div></>:events.map((i,idx)=>(
-            <EventCards key={idx} info = {i} setShow = {setShow} setCardNo = {setCardNo} cardNo = {cardNo}/>
-          ))}
-        </div>}
+          {events.length == 0 ? (
+            <>
+              <div style={{ marginLeft: "20%" }}>
+                <h2>No Events Found</h2>
+              </div>
+            </>
+          ) : (
+            events.map((i, idx) => (
+              <EventCards
+                key={idx}
+                info={i}
+                setShow={setShow}
+                setCardNo={setCardNo}
+                cardNo={cardNo}
+              />
+            ))
+          )}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default ViewEvents
+export default ViewEvents;
