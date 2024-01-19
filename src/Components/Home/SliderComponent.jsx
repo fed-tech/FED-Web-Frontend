e4ed6f097f331299b15cdbf36d02c5c7e3670541
Mohit Sharma <96767237+MohitSharma2312@users.noreply.github.com>
2024-01-19 20:42:28 +0530

    update
    added blurhash on carousel

--- src/Components/Home/SliderComponent.jsx
+++ src/Components/Home/SliderComponent.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./css/SliderComponent.css";
+import { Blurhash } from "react-blurhash";
 
 export default function SliderComponent(props) {
+  const [isImageLoaded, setImageLoaded] = useState(false);
   const redirect = useNavigate();
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = props.image;
+    img.onload = () => setImageLoaded(true);
+  }, [props.image]);
+
+
+
   return (
     <div className="carousel">
+      {!isImageLoaded && (
+        <Blurhash
+          hash="LEHV6nWB2yk8pyo0adR*.7kCMdnj"
+          width={2000}
+          height={800}
+          resolutionX={32}
+          resolutionY={32}
+          punch={1}
+        />
+      )}
       <div
         className="text"
         style={{
