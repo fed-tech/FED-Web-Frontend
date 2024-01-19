a08f432c4b725c12afd7a535e45224dc836336a5
Mohit Sharma <96767237+MohitSharma2312@users.noreply.github.com>
2024-01-19 20:42:59 +0530

    update
    removed weworkwith temporarily

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
       <KnowUsCom />
       <BelieveCom />
       <Testimonial />
-      <WorkWith />
+      {/* <WorkWith /> */}
       <LetsTalkBusiness />
     </>
   );
