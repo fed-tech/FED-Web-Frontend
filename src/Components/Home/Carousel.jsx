6261a30e03da331b3f8545ee5a657ba0d22c119b
Mohit Sharma <96767237+MohitSharma2312@users.noreply.github.com>
2024-02-08 19:59:15 +0530

    Changes

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -26,11 +26,11 @@ export default class SimpleSlider extends Component {
         <div>
             <SliderComponent
               image="https://uploads-ssl.webflow.com/64553d5a8ac054ad27b992c4/65aaaf1b9023e275f7c27cdd_65a945cc1f0bba41c914ddb0_Rectangle%2039268-min.jpg"
-              para1="FED is"
-              coloredPara=" Recruiting!"
-              para2="Join FED - Ignite Your Entrepreneurial Spirit!"
-              para3="Discover a world of innovation and creativity at the Federation of Entrepreneurship Development(FED)"
-              button="true"
+              para1="Federation of"
+              coloredPara=" Entrepreneurship"
+              para2="Development"
+              para3="The Federation of Entrepreneurship Development is the student body of KIIT TBI which aims to bring all ideas, potential startups under one umbrella ☂️"
+              button="false"
             />
           </div>
           {/* <div>
